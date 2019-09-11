9998e910de62bfbec7b9927faa1976411375487e
Rainer Hihn <hihn.rainer@gmail.com>
2019-09-11 16:22:26 +0200

    Print address + port when starting.

--- src/main/java/org/hihn/ampd/server/ServerApplication.java
+++ src/main/java/org/hihn/ampd/server/ServerApplication.java
@@ -1,12 +1,28 @@
 package org.hihn.ampd.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class ServerApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ServerApplication.class, args);
-  }
+    @Autowired
+    Environment environment;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        String address = environment.getProperty("local.server.address");
+        String port = environment.getProperty("local.server.port");
+        System.out.println("ampd is running on: "address + ":" + port);
+    }
 }

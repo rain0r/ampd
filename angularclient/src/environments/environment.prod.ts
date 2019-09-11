78a7a2187b627f3dc1dfd68a129980102e2d35cc
Rainer Hihn <hihn.rainer@gmail.com>
2019-09-11 16:20:07 +0200

    Added build script for the angularclient.

--- angularclient/src/environments/environment.prod.ts
+++ angularclient/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: true,
   coverServer: `http://${AMPD_URL}`,
   webSocketServer: `ws://${AMPD_URL}/mpd`,
-  ampdVersion: '2019-06-23 17:33:12',
+  ampdVersion: '2019-09-11T08:22:12.430Z',
 };

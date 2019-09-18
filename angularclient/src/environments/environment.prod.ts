bd256e88fc54ecceee494f21943e53c1518ee9be
Rainer Hihn <hihn.rainer@gmail.com>
2019-09-18 20:01:20 +0200

    Removed build-dependent base href

--- angularclient/src/environments/environment.prod.ts
+++ angularclient/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: true,
   coverServer: `http://${AMPD_URL}`,
   webSocketServer: `ws://${AMPD_URL}/mpd`,
-  ampdVersion: '2019-09-16 21:01',
+  ampdVersion: '2019-09-18 19:59',
 };

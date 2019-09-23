09b008ef7e65a62182636aa16a00ea51c9ea75c9
Rainer Hihn <hihn.rainer@gmail.com>
2019-09-23 20:05:01 +0200

    * Added URIEncoderPipe * Added own directory class

---
+++ src/main/java/org/hihn/ampd/server/message/outgoing/browse/Directory.java
@@ -0,0 +1,10 @@
+package org.hihn.ampd.server.message.outgoing.browse;
+
+import org.bff.javampd.file.MPDFile;
+
+public class Directory extends MPDFile {
+
+  public Directory(String path) {
+    super(path);
+  }
+}

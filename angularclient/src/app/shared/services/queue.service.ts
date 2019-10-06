7ddc4985b56bd75c376a603a7f284dae3f906166
Rainer Hihn <hihn.rainer@gmail.com>
2019-10-06 21:50:50 +0200

    Transformation to using BehaviorSubject for queue tracks

--- angularclient/src/app/shared/services/queue.service.ts
+++ angularclient/src/app/shared/services/queue.service.ts
@@ -1,18 +1,21 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, Observable, Subject} from "rxjs/index";
+import {BehaviorSubject, Observable} from "rxjs/index";
 import {QueueTrack} from "../models/queue-track";
 
 @Injectable()
 export class QueueService {
-  private resultList: Subject<QueueTrack[]> = new Subject<QueueTrack[]>();
-  public resultList$: Observable<QueueTrack[]> = this.resultList.asObservable();
+    private resultList: BehaviorSubject<QueueTrack[]> = new BehaviorSubject<QueueTrack[]>([]);
+    public resultList$: Observable<QueueTrack[]> = this.resultList.asObservable();
 
-  /* For subscribing */
-  updateResultList(updatedList) {
-    this.resultList.next(updatedList);
-  }
+    /* For subscribing */
+    addTrack(updatedList) {
+        // this.resultList.next(updatedList);
+        const currentValue = this.resultList.value;
+        const updatedValue = [...currentValue, updatedList];
+        this.resultList.next(updatedValue);
+    }
 
-  clear() {
-    this.resultList = new BehaviorSubject<QueueTrack[]>();
-  }
+    clear() {
+        this.resultList = new BehaviorSubject<QueueTrack[]>([]);
+    }
 }

8232664c74c77209a3e83624aa748c5fa0fc2cee
Rainer Hihn <hihn.rainer@gmail.com>
2019-11-01 17:51:45 +0100

    Added back-to-top button.

--- angularclient/src/app/browse/browse.component.ts
+++ angularclient/src/app/browse/browse.component.ts
@@ -1,9 +1,8 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { BrowseInfo } from '../shared/models/browse-info';
 import { MpdCommands } from '../shared/mpd/mpd-commands';
 import { BrowseService } from '../shared/services/browse.service';
-import { NotificationService } from '../shared/services/notification.service';
 import { WebSocketService } from '../shared/services/web-socket.service';
 
 @Component({
@@ -18,7 +17,6 @@ export class BrowseComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private webSocketService: WebSocketService,
-    private notificationService: NotificationService,
     private browseService: BrowseService
   ) {
     this.browseInfo = this.browseService.browseInfo;
@@ -32,35 +30,12 @@ export class BrowseComponent implements OnInit {
     });
   }
 
+  public onBackToTop(): void {
+    window.scrollTo(0, 0);
+  }
+
   public onClearQueue(): void {
     this.webSocketService.send(MpdCommands.RM_ALL);
     this.webSocketService.send(MpdCommands.GET_QUEUE);
-  }
-
-  @HostListener('click', ['$event'])
-  public onAddDir(dir: string): void {
-    if (event) {
-      event.stopPropagation();
-    }
-    if (typeof dir !== 'string') {
-      return;
-    }
-    if (dir.startsWith('/')) {
-      dir = dir.substr(1, dir.length);
-    }
-    this.webSocketService.sendData(MpdCommands.ADD_DIR, {
-      dir,
-    });
-    this.notificationService.popUp(`Added dir: "${dir}"`);
-  }
-
-  @HostListener('click', ['$event'])
-  public onPlayDir(dir: string): void {
-    if (typeof dir !== 'string') {
-      return;
-    }
-    this.onAddDir(dir);
-    this.webSocketService.send(MpdCommands.SET_PLAY);
-    this.notificationService.popUp(`Playing dir: "${dir}"`);
   }
 }

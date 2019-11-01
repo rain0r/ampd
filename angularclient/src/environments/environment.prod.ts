c83e5851b3449bc636d806f5685893b99da12175
Rainer Hihn <hihn.rainer@gmail.com>
2019-11-01 16:54:55 +0100

    Ng update.

--- angularclient/src/environments/environment.prod.ts
+++ angularclient/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: true,
   coverServer: `http://${AMPD_URL}`,
   webSocketServer: `ws://${AMPD_URL}/mpd`,
-  ampdVersion: '2019-10-07 09:30 (04c7bf00cedb75db9fead844e97da061d057226f)',
+  ampdVersion: '2019-11-01 16:54 (3a04f14314e1d224533c988219af0fbfba34af64)',
 };

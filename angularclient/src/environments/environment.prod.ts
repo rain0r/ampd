949a113b9e02f91e4a2196cd056cd5564772c0d7
Rainer Hihn <hihn.rainer@gmail.com>
2019-12-25 19:02:47 +0100

    Use same version in package.json and pom.xml

--- angularclient/src/environments/environment.prod.ts
+++ angularclient/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: true,
   coverServer: `http://${AMPD_URL}`,
   webSocketServer: `ws://${AMPD_URL}/mpd`,
-  ampdVersion: '2019-12-22 19:27 (3112f9b5d469a7feeb19215d7902c16265573bf7)',
+  ampdVersion: '0.0.7-SNAPSHOT',
 };

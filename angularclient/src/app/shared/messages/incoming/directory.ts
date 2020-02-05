b34241696a8e7f241a9eca1381053fc38e1829a7
Rainer Hihn <hihn.rainer@gmail.com>
2020-02-05 20:53:08 +0100

    Display covers.

--- angularclient/src/app/shared/messages/incoming/directory.ts
+++ angularclient/src/app/shared/messages/incoming/directory.ts
@@ -2,18 +2,15 @@ export interface IDirectory {
   directory: boolean;
   path: string;
   lastModified?: Date;
-  albumCover: string;
 }
 
 export class Directory implements IDirectory {
   public directory: boolean;
   public path: string;
-  public albumCover: string;
 
-  constructor(directory: boolean, path: string, albumCover: string) {
+  constructor(directory: boolean, path: string) {
     this.directory = directory;
     this.path = path;
-    this.albumCover = albumCover;
   }
   public displayedPath() {
     return (

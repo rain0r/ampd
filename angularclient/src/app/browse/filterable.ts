7bcd9eb272a561ebe9b387efbcea4322a82b8472
Rainer Hihn <hihn.rainer@gmail.com>
2020-02-09 16:42:18 +0100

    Finished filtering.

--- angularclient/src/app/browse/filterable.ts
+++ angularclient/src/app/browse/filterable.ts
@@ -2,7 +2,7 @@ import { Subscription } from 'rxjs/index';
 import { InternalCommands } from '../shared/commands/internal';
 import { MessageService } from '../shared/services/message.service';
 
-export abstract class FilterableImpl {
+export abstract class Filterable {
   protected filterValue = '';
   private subscription: Subscription = new Subscription();
 
@@ -11,14 +11,12 @@ export abstract class FilterableImpl {
     this.subscription = messageService.getMessage().subscribe(message => {
       if (message.text === InternalCommands.BROWSE_FILTER) {
         if (message.data && message.data.filterValue) {
-          this.filter(message.data.filterValue);
+          this.filterValue = message.data.filterValue;
+        }
+        else {
+          this.filterValue = '';
         }
       }
     });
-  }
-  protected filter(filterValue: string) {
-    if (filterValue) {
-      this.filterValue = filterValue;
-    }
   }
 }

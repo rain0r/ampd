8fc46115c00877a37ad0e395666a3f1370ca00a4
Rainer Hihn <hihn.rainer@gmail.com>
2020-04-03 17:42:38 +0200

    frontend v1.0.3

--- angularclient/src/environments/environment.prod.ts
+++ angularclient/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   production: true,
   coverServer: `http://${AMPD_URL}`,
   webSocketServer: `ws://${AMPD_URL}/mpd`,
-  ampdVersion: '1.0.2',
+  ampdVersion: '1.0.3',
   gitCommitId: 'c029f65'    
 };

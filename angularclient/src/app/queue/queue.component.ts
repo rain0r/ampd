58a32a37bd71bc4da32c6d5cb34b309b3f857a57
Rainer Hihn <hihn.rainer@gmail.com>
2020-04-03 22:46:46 +0200

    Formatting

--- angularclient/src/app/queue/queue.component.ts
+++ angularclient/src/app/queue/queue.component.ts
@@ -1,15 +1,18 @@
-import {Component, HostBinding, HostListener, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {AmpdBlockUiService} from '../shared/block/ampd-block-ui.service';
-import {InternalCommands} from '../shared/commands/internal';
-import {ControlPanelImpl, IControlPanel,} from '../shared/messages/incoming/control-panel';
-import {ServerStatusRootImpl} from '../shared/messages/incoming/state-messages';
-import {StateMsgPayload} from '../shared/messages/incoming/state-msg-payload';
-import {QueueTrack} from '../shared/models/queue-track';
-import {MpdCommands} from '../shared/mpd/mpd-commands';
-import {MessageService} from '../shared/services/message.service';
-import {WebSocketService} from '../shared/services/web-socket.service';
-import {OverlayContainer} from '@angular/cdk/overlay';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AmpdBlockUiService } from '../shared/block/ampd-block-ui.service';
+import { InternalCommands } from '../shared/commands/internal';
+import {
+  ControlPanelImpl,
+  IControlPanel,
+} from '../shared/messages/incoming/control-panel';
+import { ServerStatusRootImpl } from '../shared/messages/incoming/state-messages';
+import { StateMsgPayload } from '../shared/messages/incoming/state-msg-payload';
+import { QueueTrack } from '../shared/models/queue-track';
+import { MpdCommands } from '../shared/mpd/mpd-commands';
+import { MessageService } from '../shared/services/message.service';
+import { WebSocketService } from '../shared/services/web-socket.service';
 
 const THEME_DARKNESS_SUFFIX = `-dark`;
 
@@ -19,33 +22,34 @@ const THEME_DARKNESS_SUFFIX = `-dark`;
   styleUrls: ['./queue.component.scss'],
 })
 export class QueueComponent implements OnInit {
+  @HostBinding('class') public activeThemeCssClass: string = '';
+  public isThemeDark = false;
+  public activeTheme: string = '';
+  public themes: string[] = [
+    'deeppurple-amber',
+    'indigo-pink',
+    'pink-bluegrey',
+    'purple-green',
+  ];
   private controlPanel: IControlPanel = new ControlPanelImpl();
   private currentSong: QueueTrack = new QueueTrack();
   private volume: number = 0;
   private stateSubs: Observable<ServerStatusRootImpl>;
   private currentState: string = '';
 
-  @HostBinding('class') activeThemeCssClass: string = '';
-  isThemeDark = false;
-  activeTheme: string = '';
-  themes: string[] = [
-    'deeppurple-amber',
-    'indigo-pink',
-    'pink-bluegrey',
-    'purple-green',
-  ];
-
-  constructor(private webSocketService: WebSocketService,
-              private ampdBlockUiService: AmpdBlockUiService,
-              private messageService: MessageService,
-              private overlayContainer: OverlayContainer,) {
+  constructor(
+    private webSocketService: WebSocketService,
+    private ampdBlockUiService: AmpdBlockUiService,
+    private messageService: MessageService,
+    private overlayContainer: OverlayContainer
+  ) {
     this.ampdBlockUiService.start();
 
     this.stateSubs = this.webSocketService.getStateSubscription();
     this.buildStateReceiver();
     this.webSocketService.send(MpdCommands.GET_QUEUE);
 
-    this.setActiveTheme('deeppurple-amber', /* darkness: */ false)
+    this.setActiveTheme('deeppurple-amber', /* darkness: */ false);
   }
 
   @HostListener('document:visibilitychange', ['$event'])
@@ -62,12 +66,41 @@ export class QueueComponent implements OnInit {
     const elapsedMinutes = Math.floor(elapsedTime / 60);
     const elapsedSeconds = elapsedTime - elapsedMinutes * 60;
     return (
-        elapsedMinutes + ':' + (elapsedSeconds < 10 ? '0' : '') + elapsedSeconds
+      elapsedMinutes + ':' + (elapsedSeconds < 10 ? '0' : '') + elapsedSeconds
     );
   }
 
   public ngOnInit() {
     this.webSocketService.send(MpdCommands.GET_QUEUE);
+  }
+
+  public setActiveTheme(theme: string, darkness: boolean = false) {
+    if (darkness === null) {
+      darkness = this.isThemeDark;
+    } else if (this.isThemeDark === darkness) {
+      if (this.activeTheme === theme) {
+        return;
+      }
+    } else {
+      this.isThemeDark = darkness;
+    }
+
+    this.activeTheme = theme;
+
+    const cssClass = darkness === true ? theme + THEME_DARKNESS_SUFFIX : theme;
+
+    const classList = this.overlayContainer.getContainerElement().classList;
+    if (classList.contains(this.activeThemeCssClass)) {
+      classList.replace(this.activeThemeCssClass, cssClass);
+    } else {
+      classList.add(cssClass);
+    }
+
+    this.activeThemeCssClass = cssClass;
+  }
+
+  public toggleDarkness() {
+    this.setActiveTheme(this.activeTheme, !this.isThemeDark);
   }
 
   private buildState(payload: StateMsgPayload): void {
@@ -78,8 +111,8 @@ export class QueueComponent implements OnInit {
     /* Call buildQueue once if there is no current track set */
     if ('id' in this.currentSong === true) {
       if (
-          payload.currentSong &&
-          payload.currentSong.id !== this.currentSong.id
+        payload.currentSong &&
+        payload.currentSong.id !== this.currentSong.id
       ) {
         hasSongChanged = true;
       }
@@ -90,7 +123,7 @@ export class QueueComponent implements OnInit {
     // Build the currentSong object - holds info about the song currently played
     this.currentSong = new QueueTrack(payload.currentSong);
     this.currentSong.elapsedFormatted = this.getFormattedElapsedTime(
-        payload.serverStatus.elapsedTime
+      payload.serverStatus.elapsedTime
     );
     this.currentSong.progress = payload.serverStatus.elapsedTime;
 
@@ -113,34 +146,9 @@ export class QueueComponent implements OnInit {
         this.buildState(message.payload);
       } catch (error) {
         console.error(
-            `Error handling message: ${message.type}, error: ${error}`
+          `Error handling message: ${message.type}, error: ${error}`
         );
       }
     });
-  }
-
-  setActiveTheme(theme: string, darkness: boolean = false) {
-    if (darkness === null)
-      darkness = this.isThemeDark;
-    else if (this.isThemeDark === darkness) {
-      if (this.activeTheme === theme) return
-    } else
-      this.isThemeDark = darkness;
-
-    this.activeTheme = theme;
-
-    const cssClass = darkness === true ? theme + THEME_DARKNESS_SUFFIX : theme;
-
-    const classList = this.overlayContainer.getContainerElement().classList;
-    if (classList.contains(this.activeThemeCssClass))
-      classList.replace(this.activeThemeCssClass, cssClass);
-    else
-      classList.add(cssClass);
-
-    this.activeThemeCssClass = cssClass;
-  }
-
-  toggleDarkness() {
-    this.setActiveTheme(this.activeTheme, !this.isThemeDark)
   }
 }

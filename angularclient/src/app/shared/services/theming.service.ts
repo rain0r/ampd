70a08f8cab5e4e358c2b903d0a5ccd0eba0f8243
Rainer Hihn <hihn.rainer@gmail.com>
2020-04-04 23:01:46 +0200

    M angularclient/src/app/app.component.ts  M angularclient/src/app/shared/services/theming.service.ts

--- angularclient/src/app/shared/services/theming.service.ts
+++ angularclient/src/app/shared/services/theming.service.ts
@@ -1,21 +1,37 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs/index';
 
+const IS_DARK_MODE_KEY = 'isDarkTheme';
+
 @Injectable({
   providedIn: 'root',
 })
 export class ThemingService {
-  // public isDarkTheme = new Observable();
-
-  // private _darkTheme: Subject<boolean> = new Subject<boolean>();
-  private darkThemeSubj = new BehaviorSubject(false);
+  public darkThemeSubj = new BehaviorSubject(false);
   public isDarkTheme = this.darkThemeSubj.asObservable();
-  
+
+
   constructor() {
-    // this.isDarkTheme = this.darkThemeSubj.asObservable();
+    console.log("constructor");
+    this.darkThemeSubj.next(this.loadState());
   }
 
   public setDarkTheme(isDarkTheme: boolean) {
     this.darkThemeSubj.next(isDarkTheme);
+    this.saveState(isDarkTheme);
+  }
+
+  private loadState(): boolean {
+    try {
+      const saved: string = localStorage.getItem(IS_DARK_MODE_KEY) || '';
+      return JSON.parse(saved);
+    }
+    catch (err) {
+      return false;
+    }
+  }
+
+  private saveState(isDarkTheme: boolean): void {
+    localStorage.setItem(IS_DARK_MODE_KEY, JSON.stringify(isDarkTheme));
   }
 }

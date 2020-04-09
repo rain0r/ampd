a2b8b2bf4ca499295cdb74ba269d5217a2a317c7
Rainer Hihn <hihn.rainer@gmail.com>
2020-04-09 20:15:11 +0200

    Started working on a new build process which should be a lot more flexible.

--- angularclient/build.js
+++ angularclient/build.js
@@ -1,34 +1,80 @@
+#!/usr/bin/env node
+
 const dateFormat = require('dateformat');
 const replace = require('replace-in-file');
 const spawn = require('child_process').spawn;
+const fs = require('fs');
+const ampdVersion = require('child_process')
+  .execSync(
+    'mvn -q -Dexec.executable="echo" -Dexec.args=\'${project.version}\' --non-recursive exec:exec',
+    { cwd: '..' }
+  )
+  .toString()
+  .trim();
+const argv = require('yargs')
+  .usage('Build the ampd frontend.')
+  .option('prod')
+  .boolean('prod')
+  .default('prod', false)
+  .describe('prod', 'Is this a production build?')
+  .option('url')
+  .string('url')
+  .describe('url', 'The AMPD_URL')
+  .default('url', 'localhost')
+  .option('https')
+  .boolean('https')
+  .describe('https', 'use https instead of http')
+  .default('https', false)
+  .option('context')
+  .string('context')
+  .describe('context', 'The context path of ampd')
+  .default('context', '/').argv;
 
-const CONTEXT_PATH = '/';
-const prod = process.argv.includes('--prod');
-let ampdVersion = require('child_process')
-.execSync('mvn -q -Dexec.executable="echo" -Dexec.args=\'${project.version}\' --non-recursive exec:exec',
-    {cwd: '..'},)
-.toString().trim();
+// Git commit id
+const gitCommitId = require('child_process')
+  .execSync('git rev-parse --short HEAD')
+  .toString()
+  .trim();
 
-if (prod) {
-  ampdVersion = ampdVersion.replace('-SNAPSHOT', '');
+let http, ws;
+if (argv['https'] === true) {
+  http = 'https';
+  ws = 'wss';
+} else {
+  http = 'http';
+  ws = 'ws';
 }
 
-// Git hash
-const revision = require('child_process')
-.execSync('git rev-parse --short HEAD')
-.toString().trim();
+console.log(`Using context path: ${argv['context']}`);
+console.log(`Using ampdVersion: ${ampdVersion}`);
+console.log(`Using prod: ${argv['prod']}`);
+console.log(`Using url: ${argv['url']}`);
+console.log(`Using http: ${http}`);
+console.log(`Using ws: ${ws}`);
+console.log(`Using gitCommitId: ${gitCommitId}`);
 
-// Write down the build date
+// Copy the environment template
+fs.copyFile(
+  'src/templates/environment.prod.ts',
+  'src/environments/environment.prod.ts',
+  err => {
+    if (err) throw err;
+  }
+);
+
+// Replace some variables
 const options = {
   files: 'src/environments/environment.prod.ts',
-  from: [/ampdVersion: '.*'/, /gitCommitId: '.*',/],
-  to: [`ampdVersion: '${ampdVersion}'`, `gitCommitId: '${revision}'`],
+  from: [
+    /R_PROD/,
+    /R_AMPD_URL/g,
+    /R_HTTP_PROT/,
+    /R_WS_PROT/,
+    /R_AMPD_VERSION/,
+    /R_GIT_COMMIT_ID/,
+  ],
+  to: [argv['prod'], argv['url'], http, ws, ampdVersion, gitCommitId],
 };
-
-console.log(`Using CONTEXT_PATH ${CONTEXT_PATH}`);
-console.log(`Using ampdVersion ${ampdVersion}`);
-console.log(`Using git commit ${revision}`);
-
 try {
   replace.sync(options);
 } catch (error) {
@@ -39,12 +85,12 @@ console.log('Starting build');
 
 let child;
 
-if (prod) {
+if (argv['prod']) {
   child = spawn('ng', [
     'build',
     '--es5-browser-support',
     '--configuration=production',
-    `--base-href=${CONTEXT_PATH}`,
+    `--base-href=${argv['context']}`,
   ]);
 } else {
   child = spawn('ng', [
@@ -52,10 +98,10 @@ if (prod) {
     '--source-map',
     '--prod=false',
     '--build-optimizer=false',
-    `--base-href=${CONTEXT_PATH}`,
+    `--base-href=${argv['context']}`,
   ]);
 }
 
-child.on('error', (err) => {
+child.on('error', err => {
   console.error(err);
 });

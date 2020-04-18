c939c946dfee413ee16780e04b6762ff604c854f
Rainer Hihn <hihn.rainer@gmail.com>
2020-04-18 20:10:19 +0200

    Removed environment.prod.ts since it should be generated individually via build.js

--- angularclient/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  coverServer: 'http://localhost',
-  webSocketServer: 'ws://localhost/mpd',
-  ampdVersion: '1.1.1-SNAPSHOT',
-  gitCommitId: '5cbb2ed',
-};

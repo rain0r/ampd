3fd1213ff96ea9c479aba0581c3bf4133e00b2a4
Rainer Hihn <hihn.rainer@gmail.com>
2020-05-31 21:08:14 +0200

    M angularclient/package-lock.json  M angularclient/package.json  M angularclient/src/app/app.module.ts  M angularclient/src/app/shared/services/web-socket.service.ts

--- angularclient/src/app/shared/services/web-socket.service.ts
+++ angularclient/src/app/shared/services/web-socket.service.ts
@@ -1,25 +1,21 @@
 import { Injectable } from "@angular/core";
-import { StompService } from "@stomp/ng2-stompjs";
 import { Message } from "@stomp/stompjs";
-import { Observable } from "rxjs";
-import { filter, map } from "rxjs/internal/operators";
-import { BrowseRootImpl } from "../messages/incoming/browse";
-import { QueueRootImpl } from "../messages/incoming/queue";
-import { SearchRootImpl } from "../messages/incoming/search";
-import { ServerStatusRootImpl } from "../messages/incoming/state-messages";
+import {filter, map} from "rxjs/internal/operators";
 import { REMOTE_QUEUE } from "../mpd/mpd-commands";
-import { MpdTypes } from "../mpd/mpd-types";
+import { RxStompService } from "@stomp/ng2-stompjs";
+import { BaseResponse } from "../messages/incoming/base-response";
+import {MpdTypes} from "../mpd/mpd-types";
 
 @Injectable()
 export class WebSocketService {
-  constructor(private stompService: StompService) {}
+  constructor(private rxStompService: RxStompService) {}
 
   send(cmd: string): void {
     const data = JSON.stringify({
       type: cmd,
       payload: null,
     });
-    this.stompService.publish(REMOTE_QUEUE, data);
+    this.rxStompService.publish({ destination: REMOTE_QUEUE, body: data });
   }
 
   sendData(cmd: string, payload: Record<string, unknown>): void {
@@ -27,54 +23,52 @@ export class WebSocketService {
       type: cmd,
       payload,
     });
-    this.stompService.publish(REMOTE_QUEUE, data);
+    this.rxStompService.publish({ destination: REMOTE_QUEUE, body: data });
   }
 
-  getStateSubscription(): Observable<ServerStatusRootImpl> {
-    return this.stompService.subscribe("/topic/state").pipe(
-      map((message: Message) => message.body),
-      map((body) => JSON.parse(body)),
-      filter((body) => body !== null),
-      filter((body) => body.type === MpdTypes.STATE),
-      map((jsonObj) => {
-        return jsonObj as ServerStatusRootImpl;
-      })
-    );
+  getStateSubscription(): void {
+    const foo = this.rxStompService
+      .watch("/topic/state")
+      .pipe(
+        map((message: Message) => message.body),
+        map((body: string) => <BaseResponse>JSON.parse(body)),
+      )
+      .subscribe((foo) => console.log(foo));
   }
 
-  getQueueSubscription(): Observable<QueueRootImpl> {
-    return this.stompService.subscribe("/topic/queue").pipe(
-      map((message: Message) => message.body),
-      map((body) => JSON.parse(body)),
-      filter((body) => body !== null),
-      filter((body) => body.type === MpdTypes.QUEUE),
-      map((jsonObj) => {
-        return jsonObj as QueueRootImpl;
-      })
-    );
-  }
-
-  getBrowseSubscription(): Observable<BrowseRootImpl> {
-    return this.stompService.subscribe("/topic/controller").pipe(
-      map((message: Message) => message.body),
-      map((body) => JSON.parse(body)),
-      filter((body) => body !== null),
-      filter((body) => body.type === MpdTypes.BROWSE),
-      map((jsonObj) => {
-        return jsonObj as BrowseRootImpl;
-      })
-    );
-  }
-
-  getSearchSubscription(): Observable<SearchRootImpl> {
-    return this.stompService.subscribe("/topic/controller").pipe(
-      map((message: Message) => message.body),
-      map((body) => JSON.parse(body)),
-      filter((body) => body !== null),
-      filter((body) => body.type === MpdTypes.SEARCH_RESULTS),
-      map((jsonObj) => {
-        return jsonObj as SearchRootImpl;
-      })
-    );
-  }
+  // getQueueSubscription(): Observable<QueueRootImpl> {
+  //   return this.rxStompService.subscribe("/topic/queue").pipe(
+  //       map((message: Message) => message.body),
+  //       map((body) => JSON.parse(body)),
+  //       filter((body) => body !== null),
+  //       filter((body) => body.type === MpdTypes.QUEUE),
+  //       map((jsonObj) => {
+  //         return jsonObj as QueueRootImpl;
+  //       })
+  //   );
+  // }
+  //
+  // getBrowseSubscription(): Observable<BrowseRootImpl> {
+  //   return this.rxStompService.subscribe("/topic/controller").pipe(
+  //       map((message: Message) => message.body),
+  //       map((body) => JSON.parse(body)),
+  //       filter((body) => body !== null),
+  //       filter((body) => body.type === MpdTypes.BROWSE),
+  //       map((jsonObj) => {
+  //         return jsonObj as BrowseRootImpl;
+  //       })
+  //   );
+  // }
+  //
+  // getSearchSubscription(): Observable<SearchRootImpl> {
+  //   return this.rxStompService.subscribe("/topic/controller").pipe(
+  //       map((message: Message) => message.body),
+  //       map((body) => JSON.parse(body)),
+  //       filter((body) => body !== null),
+  //       filter((body) => body.type === MpdTypes.SEARCH_RESULTS),
+  //       map((jsonObj) => {
+  //         return jsonObj as SearchRootImpl;
+  //       })
+  //   );
+  // }
 }

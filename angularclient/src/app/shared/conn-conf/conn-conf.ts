9cab7b1aefbabdeb50a3e442b9a822e98fcc0864
Rainer Hihn <hihn.rainer@gmail.com>
2020-06-01 19:31:35 +0200

    Finished settings.

--- angularclient/src/app/shared/conn-conf/conn-conf.ts
+++ angularclient/src/app/shared/conn-conf/conn-conf.ts
@@ -1,15 +1,7 @@
 export class ConnConf {
-  private _backendAddr: string;
+  backendAddr: string;
 
   constructor(backendAddr: string) {
-    this._backendAddr = backendAddr;
-  }
-
-  get backendAddr(): string {
-    return this._backendAddr;
-  }
-
-  set backendAddr(value: string) {
-    this._backendAddr = value;
+    this.backendAddr = backendAddr;
   }
 }
--- angularclient/src/app/shared/cover-size/responsive-cover-size.service.spec.ts
+++ angularclient/src/app/shared/cover-size/responsive-cover-size.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ResponsiveCoverSizeService } from './responsive-cover-size.service';
+import {ResponsiveCoverSizeService} from './responsive-cover-size.service';
 
 describe('ResponsiveCoverSizeService', () => {
   let service: ResponsiveCoverSizeService;

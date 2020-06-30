910e3204edef9be1ec3879831a20db5e92b4c0cb
Rainer Hihn <hihn.rainer@gmail.com>
2020-06-30 18:49:24 +0200

    Adding sort to table cols

--- angularclient/src/app/search/search.component.ts
+++ angularclient/src/app/search/search.component.ts
@@ -1,6 +1,4 @@
-import { Component } from "@angular/core";
-
-import { Observable } from "rxjs";
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from "@angular/core";
 
 import { MpdTrack } from "../shared/messages/incoming/mpd-track";
 import {
@@ -12,32 +10,67 @@ import { MpdCommands } from "../shared/mpd/mpd-commands";
 import { WebSocketService } from "../shared/services/web-socket.service";
 import { DeviceDetectorService } from "ngx-device-detector";
 import { NotificationService } from "../shared/services/notification.service";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: "app-search",
   templateUrl: "./search.component.html",
   styleUrls: ["./search.component.scss"],
 })
-export class SearchComponent {
-  search = "";
-  searchSubs: Observable<ISearchMsgPayload>;
-  titleQueue: QueueTrack[] = [];
+export class SearchComponent implements OnInit {
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  oldDataSource: MatTableDataSource<QueueTrack> = new MatTableDataSource<
+    QueueTrack
+  >([]);
   searchResultCount = 0;
-  private displayedColumns = [
+  search = "";
+  spinnerVisible = false;
+
+  private oldDisplayedColumns = [
     { name: "artistName", showMobile: true },
     { name: "albumName", showMobile: false },
     { name: "title", showMobile: true },
     { name: "length", showMobile: false },
-    { name: "action", showMobile: true },
+    { name: "addTitle", showMobile: true },
+    { name: "playTitle", showMobile: true },
   ];
 
   constructor(
+    private ref: ChangeDetectorRef,
     private notificationService: NotificationService,
     private webSocketService: WebSocketService,
     private deviceService: DeviceDetectorService
   ) {
-    this.searchSubs = this.webSocketService.getSearchSubscription();
     this.getResults();
+  }
+
+  ngOnInit() {
+    this.oldDataSource.sort = this.sort;
   }
 
   onPlayTitle(track: MpdTrack): void {
@@ -68,6 +101,7 @@ export class SearchComponent {
         this.webSocketService.sendData(MpdCommands.SEARCH, {
           query: searchValue,
         });
+        this.spinnerVisible = true;
       }
     } else {
       this.resetSearch();
@@ -75,13 +109,13 @@ export class SearchComponent {
   }
 
   resetSearch(): void {
-    this.titleQueue = [];
+    this.oldDataSource.data = [];
     this.searchResultCount = 0;
   }
 
   getDisplayedColumns(): string[] {
     const isMobile = this.deviceService.isMobile();
-    return this.displayedColumns
+    return this.oldDisplayedColumns
       .filter((cd) => !isMobile || cd.showMobile)
       .map((cd) => cd.name);
   }
@@ -90,12 +124,14 @@ export class SearchComponent {
    * Listen for results on the websocket channel
    */
   private getResults(): void {
-    this.searchSubs.subscribe((message: ISearchMsgPayload) =>
-      this.processSearchResults(
-        message.searchResults,
-        message.searchResultCount
-      )
-    );
+    this.webSocketService
+      .getSearchSubscription()
+      .subscribe((message: ISearchMsgPayload) =>
+        this.processSearchResults(
+          message.searchResults,
+          message.searchResultCount
+        )
+      );
   }
 
   private processSearchResults(
@@ -103,9 +139,16 @@ export class SearchComponent {
     searchResultCount: number
   ): void {
     this.resetSearch();
+    const tableData = [];
     searchResults.forEach((track: ISearchResult) => {
-      this.titleQueue.push(new QueueTrack(track));
+      tableData.push(new QueueTrack(track));
     });
+
+    this.oldDataSource = new MatTableDataSource<QueueTrack>(tableData);
+    this.oldDataSource.sort = this.sort;
+
     this.searchResultCount = searchResultCount;
+    this.spinnerVisible = false;
+    this.ref.detectChanges();
   }
 }

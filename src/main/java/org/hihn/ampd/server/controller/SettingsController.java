7e2a5f806ab3e643498371a0cf6413d2ee80faf9
Rainer Hihn <hihn.rainer@gmail.com>
2020-07-02 21:17:00 +0200

    Allow @CrossOrigin

--- src/main/java/org/hihn/ampd/server/controller/SettingsController.java
+++ src/main/java/org/hihn/ampd/server/controller/SettingsController.java
@@ -2,12 +2,14 @@ package org.hihn.ampd.server.controller;
 
 import org.hihn.ampd.server.model.Settings;
 import org.hihn.ampd.server.model.SettingsBean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class SettingsController {
 
   private final SettingsBean settingsBean;

314d40d1248286c898ff5ebe411841eb4021731d
Rainer Hihn <hihn.rainer@gmail.com>
2020-07-02 18:00:40 +0200

    Reorganized AmpdUtils

--- src/main/java/org/hihn/ampd/server/service/CoverCacheService.java
+++ src/main/java/org/hihn/ampd/server/service/CoverCacheService.java
@@ -1,17 +1,18 @@
 package org.hihn.ampd.server.service;
 
-import static org.hihn.ampd.server.util.AmpdUtils.loadFile;
-
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.Normalizer;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 import org.hihn.ampd.server.model.CoverType;
 import org.hihn.ampd.server.model.SettingsBean;
-import org.hihn.ampd.server.util.AmpdUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -26,15 +27,35 @@ public class CoverCacheService {
 
   private final SettingsBean settingsBean;
 
-  private Optional<Path> chacheDir;
+  private final Optional<Path> chacheDir;
 
   /**
    * Name of the dir that holds all covers
    */
-  private static final String CACHE_DIR = "covers";
+  private static final String CACHE_DIR_NAME = "covers";
 
   public CoverCacheService(SettingsBean settingsBean) {
     this.settingsBean = settingsBean;
+    this.chacheDir = this.buildCacheDir();
+  }
+
+  /**
+   * Scans given dir for covers.
+   *
+   * @param path The dir to search for covers.
+   * @return A list of found covers.
+   */
+  @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(
+      value = "RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE",
+      justification = "It's a java 11 compiler bug")
+  public List<Path> scanDir(Path path) {
+    List<Path> covers = new ArrayList<>();
+    try (DirectoryStream<Path> stream = Files.newDirectoryStream(path, "cover.{jpg,jpeg,png}")) {
+      stream.forEach(file -> covers.add(file));
+    } catch (IOException e) {
+      LOG.debug("No covers found in: {}", path);
+    }
+    return covers;
   }
 
   /**
@@ -51,10 +72,10 @@ public class CoverCacheService {
     }
 
     String fileName = buildFileName(coverType, artist, titleOrAlbum);
-    Path fullPath = Paths.get(this.chacheDir.get().toString(), CACHE_DIR, fileName)
+    Path fullPath = Paths.get(this.chacheDir.get().toString(), CACHE_DIR_NAME, fileName)
         .toAbsolutePath();
     try {
-      return Optional.of(loadFile(fullPath));
+      return loadFile(fullPath);
     } catch (Exception e) {
       return Optional.empty();
     }
@@ -97,13 +118,24 @@ public class CoverCacheService {
   public Optional<byte[]> loadFileAsResource(String trackFilePath) {
     Optional<Path> coverFile = findCoverFileName(trackFilePath);
     Optional<byte[]> ret = Optional.empty();
-
-    /* Load the file */
     if (coverFile.isPresent()) {
-      ret = Optional.of(loadFile(coverFile.get()));
+      ret = loadFile(coverFile.get());
     }
-
     return ret;
+  }
+
+  /**
+   * Reads a file from disk.
+   *
+   * @param path The path of a file.
+   * @return The bytes of the file.
+   */
+  public Optional<byte[]> loadFile(Path path) {
+    try {
+      return Optional.of(Files.readAllBytes(path));
+    } catch (IOException e) {
+      return Optional.empty();
+    }
   }
 
   @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(
@@ -118,11 +150,7 @@ public class CoverCacheService {
     Optional<Path> ret = Optional.empty();
     Path path = Paths.get(settingsBean.getMusicDirectory(), trackFilePath);
 
-    try {
-      if (path.getParent() == null || !path.toFile().exists()) {
-        throw new Exception();
-      }
-    } catch (Exception e) {
+    if (path.getParent() == null || !path.toFile().exists()) {
       LOG.error("No valid path: '{}'", path);
       return Optional.empty();
     }
@@ -130,7 +158,7 @@ public class CoverCacheService {
     if (path.getParent() == null) {
       return Optional.empty();
     }
-    List<Path> covers = AmpdUtils.scanDir(path.getParent());
+    List<Path> covers = scanDir(path.getParent());
 
     if (covers.size() > 0) {
       ret = Optional.of(covers.get(0));
@@ -141,19 +169,35 @@ public class CoverCacheService {
 
   private String buildFileName(CoverType coverType, String artist, String titleOrAlbum) {
     return coverType.getPrefix()
-        + AmpdUtils.stripAccents(artist)
+        + stripAccents(artist)
         + "_"
-        + AmpdUtils.stripAccents(titleOrAlbum)
+        + stripAccents(titleOrAlbum)
         + ".jpg";
   }
 
+  /**
+   * Strips all unpleasant characters from a string.
+   *
+   * @param input Input string to strip.
+   * @return The stripped string.
+   */
+  private String stripAccents(final String input) {
+    if (input == null) {
+      return null;
+    }
+    final Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+"); // $NON-NLS-1$
+    final StringBuilder decomposed =
+        new StringBuilder(Normalizer.normalize(input, Normalizer.Form.NFD));
+    return pattern.matcher(decomposed).replaceAll("");
+  }
+
   private Optional<Path> buildCacheDir() {
-    if (!this.useCache()) {
+    if (!this.settingsBean.isLocalCoverCache()) {
       return Optional.empty();
     }
 
     Path cacheDirPath = Paths
-        .get(System.getProperty("user.home"), ".local", "share", "ampd", CACHE_DIR);
+        .get(System.getProperty("user.home"), ".local", "share", "ampd", CACHE_DIR_NAME);
 
     // create ampd home
     if (!Files.exists(cacheDirPath) && !new File(cacheDirPath.toString()).mkdirs()) {
@@ -168,7 +212,6 @@ public class CoverCacheService {
   }
 
   private boolean useCache() {
-    this.chacheDir = buildCacheDir();
     return this.settingsBean.isLocalCoverCache() && this.chacheDir.isPresent();
   }
 

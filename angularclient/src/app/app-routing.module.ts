7f469f36b4f2d9f6c2e5f3d5c995e03ed1a77d64
Rainer Hihn <hihn.rainer@gmail.com>
2020-07-02 17:44:42 +0200

    Redirect to queue when access via invalid path context.

--- angularclient/src/app/app-routing.module.ts
+++ angularclient/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: "browse", component: BrowseComponent },
   { path: "search", component: SearchComponent },
   { path: "settings", component: SettingsComponent },
+  { path: '**', redirectTo: ''}
 ];
 
 @NgModule({

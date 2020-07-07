7889c073fe7251b6f99bf3752b3a6eb3a0e11daa
Rainer Hihn <hihn.rainer@gmail.com>
2020-07-07 21:45:41 +0200

    Organizing imports.

--- angularclient/src/app/shared/save-playlist-modal/save-playlist-modal.component.ts
+++ angularclient/src/app/shared/save-playlist-modal/save-playlist-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { Observable } from "rxjs";
 import { SettingsService } from "../services/settings.service";
 import { OverlayContainer } from "@angular/cdk/overlay";

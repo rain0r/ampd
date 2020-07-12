133a27718f8236e20f0ae068704a426e04089794
Rainer Hihn <hihn.rainer@gmail.com>
2020-07-12 15:09:33 +0200

    use getFindDirCoverUrl()

--- angularclient/src/app/shared/messages/incoming/directory-impl.ts
+++ angularclient/src/app/shared/messages/incoming/directory-impl.ts
@@ -20,7 +20,7 @@ export class DirectoryImpl implements Directory {
   }
 
   coverUrl(): string {
-    return `${ConnConfUtil.getFindTrackCoverUrl()}?path=${encodeURIComponent(
+    return `${ConnConfUtil.getFindDirCoverUrl()}?path=${encodeURIComponent(
       this.path
     )}`;
   }

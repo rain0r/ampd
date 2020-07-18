8a458a2c61e8265efc7747012e513436273f68ac
Rainer Hihn <hihn.rainer@gmail.com>
2020-07-18 22:35:21 +0200

    Formatting

--- src/main/java/org/hihn/ampd/server/controller/SettingsController.java
+++ src/main/java/org/hihn/ampd/server/controller/SettingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Set;
+
 @RestController
 @RequestMapping("/api")
 @CrossOrigin

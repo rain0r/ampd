772613b6656ec0611144229a2b844b77bf44f696
Rainer Hihn <hihn.rainer@gmail.com>
2020-08-22 14:26:04 +0200

    Fixed parserOptions.project

--- angularclient/.eslintrc.js
+++ angularclient/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.base.json'],
   },
   plugins: ['@typescript-eslint', 'prettier'],
   extends: [

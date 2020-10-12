f6d243f8dcdc983ca23732d16ee8b82216a09068
Rainer Hihn <hihn.rainer@gmail.com>
2020-10-12 18:03:16 +0200

    Some dependency updates

--- angularclient/.eslintrc.js
+++ angularclient/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.base.json'],
+    project: ['./tsconfig.json'],
   },
   plugins: ['@typescript-eslint', 'prettier'],
   extends: [

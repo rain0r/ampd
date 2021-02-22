92ae9ae6463147c788dffcff4aab6a25ba47a007
Rainer Hihn <hihn.rainer@gmail.com>
2021-02-22 21:26:07 +0100

    Fixed warnings

--- angularclient/src/app/ampd-rx-stomp-config.service.ts
+++ angularclient/src/app/ampd-rx-stomp-config.service.ts
@@ -1,46 +1,46 @@
-import {InjectableRxStompConfig} from "@stomp/ng2-stompjs";
-import {ApiEndpoints} from "./shared/api-endpoints";
-import {Injectable} from "@angular/core";
-import {Location} from "@angular/common";
+import { InjectableRxStompConfig } from "@stomp/ng2-stompjs";
+import { ApiEndpoints } from "./shared/api-endpoints";
+import { Injectable } from "@angular/core";
+import { Location } from "@angular/common";
 
 @Injectable()
 export class AmpdRxStompConfig extends InjectableRxStompConfig {
-    constructor(private location: Location) {
-        super();
-        // Which server?
-        this.brokerURL = this.getBrokerUrl();
-        // Headers
-        // Typical keys: login, passcode, host
-        this.connectHeaders = {};
-        // How often to heartbeat?
-        // Interval in milliseconds, set to 0 to disable
-        this.heartbeatIncoming = 0; // Typical value 0 - disabled
-        this.heartbeatOutgoing = 1000; // Typical value 20000 - every 20 seconds
-        // Wait in milliseconds before attempting auto reconnect
-        // Set to 0 to disable
-        // Typical value 500 (500 milli seconds)
-        this.reconnectDelay = 100;
-        // Will log diagnostics on console
-        // It can be quite verbose, not recommended in production
-        // Skip this key to stop logging to console
-        // this.debug = (msg: string): void => {
-        //     console.log(new Date(), msg);
-        // };
-    }
+  constructor(private location: Location) {
+    super();
+    // Which server?
+    this.brokerURL = this.getBrokerUrl();
+    // Headers
+    // Typical keys: login, passcode, host
+    this.connectHeaders = {};
+    // How often to heartbeat?
+    // Interval in milliseconds, set to 0 to disable
+    this.heartbeatIncoming = 0; // Typical value 0 - disabled
+    this.heartbeatOutgoing = 1000; // Typical value 20000 - every 20 seconds
+    // Wait in milliseconds before attempting auto reconnect
+    // Set to 0 to disable
+    // Typical value 500 (500 milli seconds)
+    this.reconnectDelay = 100;
+    // Will log diagnostics on console
+    // It can be quite verbose, not recommended in production
+    // Skip this key to stop logging to console
+    // this.debug = (msg: string): void => {
+    //     console.log(new Date(), msg);
+    // };
+  }
 
-    /**
-     * Loads the backendAddr from localStore and converts it into a format that can be used by the
-     * websocket library.
-     */
-    private getBrokerUrl(): string {
-        let brokerUrl = `${ApiEndpoints.getBackendAddr()}${this.location.prepareExternalUrl(
-            "mpd"
-        )}`;
-        if (brokerUrl.includes("https")) {
-            brokerUrl = brokerUrl.replace("https", "wss");
-        } else {
-            brokerUrl = brokerUrl.replace("http", "ws");
-        }
-        return brokerUrl;
+  /**
+   * Loads the backendAddr from localStore and converts it into a format that can be used by the
+   * websocket library.
+   */
+  private getBrokerUrl(): string {
+    let brokerUrl = `${ApiEndpoints.getBackendAddr()}${this.location.prepareExternalUrl(
+      "mpd"
+    )}`;
+    if (brokerUrl.includes("https")) {
+      brokerUrl = brokerUrl.replace("https", "wss");
+    } else {
+      brokerUrl = brokerUrl.replace("http", "ws");
     }
+    return brokerUrl;
+  }
 }
--- angularclient/src/app/search/search.component.ts
+++ angularclient/src/app/search/search.component.ts
@@ -1,128 +1,131 @@
-import {Component} from "@angular/core";
-import {SearchMsgPayload, SearchResult,} from "../shared/messages/incoming/search";
-import {QueueTrack} from "../shared/models/queue-track";
-import {WebSocketService} from "../shared/services/web-socket.service";
-import {DeviceDetectorService} from "ngx-device-detector";
-import {MatTableDataSource} from "@angular/material/table";
-import {TrackTableData} from "../shared/track-table/track-table-data";
-import {MpdCommands} from "../shared/mpd/mpd-commands.enum";
-import {ClickActions} from "../shared/track-table/click-actions.enum";
-import {NotificationService} from "../shared/services/notification.service";
+import { Component } from "@angular/core";
+import {
+  SearchMsgPayload,
+  SearchResult,
+} from "../shared/messages/incoming/search";
+import { QueueTrack } from "../shared/models/queue-track";
+import { WebSocketService } from "../shared/services/web-socket.service";
+import { DeviceDetectorService } from "ngx-device-detector";
+import { MatTableDataSource } from "@angular/material/table";
+import { TrackTableData } from "../shared/track-table/track-table-data";
+import { MpdCommands } from "../shared/mpd/mpd-commands.enum";
+import { ClickActions } from "../shared/track-table/click-actions.enum";
+import { NotificationService } from "../shared/services/notification.service";
 
 @Component({
-    selector: "app-search",
-    templateUrl: "./search.component.html",
-    styleUrls: ["./search.component.scss"],
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.scss"],
 })
 export class SearchComponent {
-    dataSource = new MatTableDataSource<QueueTrack>([]);
-    searchResultCount = 0;
-    search = "";
-    spinnerVisible = false;
-    trackTableData = new TrackTableData();
-    private searchResultTracks = [];
+  dataSource = new MatTableDataSource<QueueTrack>([]);
+  searchResultCount = 0;
+  search = "";
+  spinnerVisible = false;
+  trackTableData = new TrackTableData();
+  private searchResultTracks = [];
 
-    constructor(
-        private webSocketService: WebSocketService,
-        private deviceService: DeviceDetectorService,
-        private notificationService: NotificationService
-    ) {
-        this.buildMsgReceiver();
-    }
+  constructor(
+    private webSocketService: WebSocketService,
+    private deviceService: DeviceDetectorService,
+    private notificationService: NotificationService
+  ) {
+    this.buildMsgReceiver();
+  }
 
-    applySearch(searchValue: string): void {
-        if (searchValue) {
-            // Only search when the term is at least 3 chars long
-            if (searchValue.length > 2) {
-                this.webSocketService.sendData(MpdCommands.SEARCH, {
-                    query: searchValue,
-                });
-                this.spinnerVisible = true;
-            }
-        } else {
-            this.resetSearch();
-        }
-    }
-
-    resetSearch(): void {
-        this.dataSource.data = [];
-        this.searchResultCount = 0;
-    }
-
-    /**
-     * Listen for results on the websocket channel
-     */
-    private buildMsgReceiver(): void {
-        this.webSocketService
-            .getSearchSubscription()
-            .subscribe((message: SearchMsgPayload) =>
-                this.processSearchResults(
-                    message.searchResults,
-                    message.searchResultCount
-                )
-            );
-    }
-
-    private processSearchResults(
-        searchResults: SearchResult[],
-        searchResultCount: number
-    ): void {
-        this.resetSearch();
-        // const tracks = [];
-        this.searchResultTracks = [];
-        searchResults.forEach((track: SearchResult) =>
-            this.searchResultTracks.push(new QueueTrack(track))
-        );
-        this.dataSource = new MatTableDataSource<QueueTrack>(
-            this.searchResultTracks
-        );
-        this.trackTableData = this.buildTableData();
-        this.searchResultCount = searchResultCount;
-        this.spinnerVisible = false;
-    }
-
-    private buildTableData() {
-        const trackTable = new TrackTableData();
-        trackTable.addTitleColumn = true;
-        trackTable.clickable = true;
-        trackTable.dataSource = this.dataSource;
-        trackTable.displayedColumns = this.getDisplayedColumns();
-        trackTable.onPlayClick = ClickActions.AddPlayTrack;
-        trackTable.notify = true;
-        trackTable.pagination = true;
-        trackTable.playTitleColumn = true;
-        trackTable.sortable = true;
-        return trackTable;
-    }
-
-    private getDisplayedColumns(): string[] {
-        const isMobile = this.deviceService.isMobile();
-        const displayedColumns = [
-            {name: "artistName", showMobile: true},
-            {name: "albumName", showMobile: false},
-            {name: "title", showMobile: true},
-            {name: "length", showMobile: false},
-            {name: "addTitle", showMobile: true},
-            {name: "playTitle", showMobile: true},
-        ];
-        return displayedColumns
-            .filter((cd) => !isMobile || cd.showMobile)
-            .map((cd) => cd.name);
-    }
-
-    onAddAll() {
-        const filePaths = [];
-        this.searchResultTracks.forEach((file) => {
-            filePaths.push(file.file)
+  applySearch(searchValue: string): void {
+    if (searchValue) {
+      // Only search when the term is at least 3 chars long
+      if (searchValue.length > 2) {
+        this.webSocketService.sendData(MpdCommands.SEARCH, {
+          query: searchValue,
         });
-        this.webSocketService.sendData(MpdCommands.ADD_TRACKS, {
-            tracks: filePaths,
-        });
+        this.spinnerVisible = true;
+      }
+    } else {
+      this.resetSearch();
     }
+  }
 
-    onClearQueue(): void {
-        this.webSocketService.send(MpdCommands.RM_ALL);
-        this.webSocketService.send(MpdCommands.GET_QUEUE);
-        this.notificationService.popUp("Cleared queue");
-    }
+  resetSearch(): void {
+    this.dataSource.data = [];
+    this.searchResultCount = 0;
+  }
+
+  onAddAll(): void {
+    const filePaths = [];
+    this.searchResultTracks.forEach((file: QueueTrack) => {
+      filePaths.push(file.file);
+    });
+    this.webSocketService.sendData(MpdCommands.ADD_TRACKS, {
+      tracks: filePaths,
+    });
+  }
+
+  onClearQueue(): void {
+    this.webSocketService.send(MpdCommands.RM_ALL);
+    this.webSocketService.send(MpdCommands.GET_QUEUE);
+    this.notificationService.popUp("Cleared queue");
+  }
+
+  /**
+   * Listen for results on the websocket channel
+   */
+  private buildMsgReceiver(): void {
+    this.webSocketService
+      .getSearchSubscription()
+      .subscribe((message: SearchMsgPayload) =>
+        this.processSearchResults(
+          message.searchResults,
+          message.searchResultCount
+        )
+      );
+  }
+
+  private processSearchResults(
+    searchResults: SearchResult[],
+    searchResultCount: number
+  ): void {
+    this.resetSearch();
+    // const tracks = [];
+    this.searchResultTracks = [];
+    searchResults.forEach((track: SearchResult) =>
+      this.searchResultTracks.push(new QueueTrack(track))
+    );
+    this.dataSource = new MatTableDataSource<QueueTrack>(
+      this.searchResultTracks
+    );
+    this.trackTableData = this.buildTableData();
+    this.searchResultCount = searchResultCount;
+    this.spinnerVisible = false;
+  }
+
+  private buildTableData() {
+    const trackTable = new TrackTableData();
+    trackTable.addTitleColumn = true;
+    trackTable.clickable = true;
+    trackTable.dataSource = this.dataSource;
+    trackTable.displayedColumns = this.getDisplayedColumns();
+    trackTable.onPlayClick = ClickActions.AddPlayTrack;
+    trackTable.notify = true;
+    trackTable.pagination = true;
+    trackTable.playTitleColumn = true;
+    trackTable.sortable = true;
+    return trackTable;
+  }
+
+  private getDisplayedColumns(): string[] {
+    const isMobile = this.deviceService.isMobile();
+    const displayedColumns = [
+      { name: "artistName", showMobile: true },
+      { name: "albumName", showMobile: false },
+      { name: "title", showMobile: true },
+      { name: "length", showMobile: false },
+      { name: "addTitle", showMobile: true },
+      { name: "playTitle", showMobile: true },
+    ];
+    return displayedColumns
+      .filter((cd) => !isMobile || cd.showMobile)
+      .map((cd) => cd.name);
+  }
 }

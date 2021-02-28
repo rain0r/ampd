e4585a55fdfb9ea9c6d36e0c74fb87f027888306
Rainer Hihn <hihn.rainer@gmail.com>
2021-02-28 18:46:41 +0100

    Renaming

--- angularclient/src/app/browse/directories/directories.component.ts
+++ angularclient/src/app/browse/directories/directories.component.ts
@@ -1,15 +1,15 @@
-import { Component, Input } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { MessageService } from "../../shared/services/message.service";
-import { NotificationService } from "../../shared/services/notification.service";
-import { WebSocketService } from "../../shared/services/web-socket.service";
-import { Filterable } from "../filterable";
-import { MpdCommands } from "../../shared/mpd/mpd-commands.enum";
+import {Component, Input} from "@angular/core";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MessageService} from "../../shared/services/message.service";
+import {NotificationService} from "../../shared/services/notification.service";
+import {WebSocketService} from "../../shared/services/web-socket.service";
+import {Filterable} from "../filterable";
+import {MpdCommands} from "../../shared/mpd/mpd-commands.enum";
 
-import { ResponsiveCoverSizeService } from "../../shared/services/responsive-cover-size.service";
-import { BehaviorSubject, Observable } from "rxjs";
-import { SettingsService } from "../../shared/services/settings.service";
-import { Directory } from "../../shared/messages/incoming/directory";
+import {ResponsiveCoverSizeService} from "../../shared/services/responsive-cover-size.service";
+import {BehaviorSubject, Observable} from "rxjs";
+import {SettingsService} from "../../shared/services/settings.service";
+import {Directory} from "../../shared/messages/incoming/directory";
 
 @Component({
   selector: "app-directories",
@@ -19,17 +19,20 @@ import { Directory } from "../../shared/messages/incoming/directory";
 export class DirectoriesComponent extends Filterable {
   @Input() dirQueue: Directory[] = [];
   coverSizeClass: Observable<string>;
+  /**
+   * The get parameter 'dir', holding the currently viewed directory.
+   */
   dirQueryParam = new BehaviorSubject<string>("/");
   maxCoversDisplayed = 50;
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    private webSocketService: WebSocketService,
-    private notificationService: NotificationService,
-    private messageService: MessageService,
-    private responsiveCoverSizeService: ResponsiveCoverSizeService,
-    private router: Router,
-    private settingsService: SettingsService
+      private activatedRoute: ActivatedRoute,
+      private webSocketService: WebSocketService,
+      private notificationService: NotificationService,
+      private messageService: MessageService,
+      private responsiveCoverSizeService: ResponsiveCoverSizeService,
+      private router: Router,
+      private settingsService: SettingsService
   ) {
     super(messageService);
     this.coverSizeClass = responsiveCoverSizeService.getCoverCssClass();
@@ -42,7 +45,7 @@ export class DirectoriesComponent extends Filterable {
     });
   }
 
-  getParamDir(): Observable<string> {
+  getDirQueryParam(): Observable<string> {
     return this.dirQueryParam.asObservable();
   }
 
@@ -66,13 +69,13 @@ export class DirectoriesComponent extends Filterable {
 
   onRowClick(dir: Directory): void {
     this.router
-      .navigate(["browse"], { queryParams: { dir: dir.path } })
-      .catch(() => void 0);
+    .navigate(["browse"], {queryParams: {dir: dir.path}})
+    .catch(() => void 0);
   }
 
   getAlbumCoverUrl(path: string): string {
     return `${this.settingsService.getFindDirCoverUrl()}?path=${encodeURIComponent(
-      path
+        path
     )}`;
   }
 

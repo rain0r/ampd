fcbde794ad6be87ce87ea251c7f2e8585bf0b96c
Rainer Hihn <hihn.rainer@gmail.com>
2021-03-03 16:59:04 +0100

    Added a modal to show all shortcuts available

---
+++ angularclient/src/app/navbar/help-dialog/help-modal.component.ts
@@ -0,0 +1,36 @@
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Component({
+  selector: "app-help-dialog",
+  templateUrl: "./help-modal.component.html",
+  styleUrls: ["./help-modal.component.scss"],
+})
+export class HelpModalComponent {
+  isDarkTheme: Observable<boolean>;
+  shortcuts = [
+    // General
+    new Map([["f", "Jump to the filter input field"]]),
+    // Player controls
+    new Map([
+      ["p", "Play / Pause"],
+      ["←", "Previous song"],
+      ["→", "Next song"],
+    ]),
+    // Navigation
+    new Map([
+      ["1", "Navigate to the queue view"],
+      ["2", "Navigate to the browse view"],
+      ["3", "Navigate to the search view"],
+      ["4", "Navigate to the settings view"],
+    ]),
+    new Map([
+      // MPD mode
+      ["R", "Toggle consume"],
+      ["r", "Toggle repeat"],
+      ["x", "Toggle crossfade"],
+      ["y", "Toggle single"],
+      ["z", "Toggle shuffle"],
+    ]),
+  ];
+}

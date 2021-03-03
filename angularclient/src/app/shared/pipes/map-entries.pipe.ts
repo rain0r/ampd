865b115563b74f8864d2f960de110645723e3dda
Rainer Hihn <hihn.rainer@gmail.com>
2021-03-03 17:00:39 +0100

    a pipe to iterate over a map while preserving the order of the items

---
+++ angularclient/src/app/shared/pipes/map-entries.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from "@angular/core";
+import { KeyValue } from "@angular/common";
+
+@Pipe({
+  name: "mapEntries",
+})
+export class MapEntriesPipe implements PipeTransform {
+  transform<K, V>(input: Map<K, V>): ReadonlyArray<KeyValue<K, V>> {
+    return Array.from(input ?? []).map(([key, value]) => ({ key, value }));
+  }
+}

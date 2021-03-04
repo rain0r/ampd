a2dee000797cd5def323c1ec83590436d4882f5f
Rainer Hihn <hihn.rainer@gmail.com>
2021-03-04 20:06:59 +0100

    M angularclient/src/app/queue/queue-header/queue-header.component.html  M angularclient/src/app/queue/queue-header/queue-header.component.ts  M angularclient/src/app/shared/services/mpd.service.ts

--- angularclient/src/app/queue/queue-header/queue-header.component.ts
+++ angularclient/src/app/queue/queue-header/queue-header.component.ts
@@ -1,16 +1,16 @@
-import { HttpClient } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
+import {HttpClient} from "@angular/common/http";
+import {Component, OnInit} from "@angular/core";
+import {MatDialog} from "@angular/material/dialog";
 
-import { ResponsiveCoverSizeService } from "../../shared/services/responsive-cover-size.service";
-import { QueueTrack } from "../../shared/models/queue-track";
-import { MpdService } from "../../shared/services/mpd.service";
-import { filter, take } from "rxjs/operators";
-import { CoverModalComponent } from "../cover-modal/cover-modal.component";
-import { SettingsService } from "../../shared/services/settings.service";
-import { MessageService } from "../../shared/services/message.service";
-import { InternalMessageType } from "../../shared/messages/internal/internal-message-type.enum";
-import { BehaviorSubject, combineLatest, Observable } from "rxjs";
+import {ResponsiveCoverSizeService} from "../../shared/services/responsive-cover-size.service";
+import {QueueTrack} from "../../shared/models/queue-track";
+import {MpdService} from "../../shared/services/mpd.service";
+import {filter, take} from "rxjs/operators";
+import {CoverModalComponent} from "../cover-modal/cover-modal.component";
+import {SettingsService} from "../../shared/services/settings.service";
+import {MessageService} from "../../shared/services/message.service";
+import {InternalMessageType} from "../../shared/messages/internal/internal-message-type.enum";
+import {BehaviorSubject, combineLatest, Observable} from "rxjs";
 
 @Component({
   selector: "app-queue-header",
@@ -25,12 +25,12 @@ export class QueueHeaderComponent implements OnInit {
   private displayCover$ = new BehaviorSubject<boolean>(false);
 
   constructor(
-    private dialog: MatDialog,
-    private http: HttpClient,
-    private responsiveCoverSizeService: ResponsiveCoverSizeService,
-    private mpdService: MpdService,
-    private settingsService: SettingsService,
-    private messageService: MessageService
+      private dialog: MatDialog,
+      private http: HttpClient,
+      private responsiveCoverSizeService: ResponsiveCoverSizeService,
+      private mpdService: MpdService,
+      private settingsService: SettingsService,
+      private messageService: MessageService
   ) {
     this.isDisplayCover = this.displayCover$.asObservable();
     this.coverSizeClass = responsiveCoverSizeService.getCoverCssClass();
@@ -56,25 +56,25 @@ export class QueueHeaderComponent implements OnInit {
       return;
     }
     this.http
-      .head(this.currentTrack.coverUrl, { observe: "response" })
-      .subscribe(
+    .head(this.currentTrack.coverUrl, {observe: "response"})
+    .subscribe(
         () => void 0,
         () => this.displayCover$.next(false),
         () => this.coverAvailable()
-      );
+    );
   }
 
   private coverAvailable(): void {
     combineLatest([this.currentState, this.settingsService.isDisplayCovers])
-      .pipe(take(1))
-      .subscribe((result) => {
-        if (
+    .pipe(take(1))
+    .subscribe((result) => {
+      if (
           result[0] !== "stop" && // Check state, we don't change the cover if the player has stopped
           result[1] === true // Check if cover-display is active in the frontend-settings
-        ) {
-          this.displayCover$.next(true);
-        }
-      });
+      ) {
+        this.displayCover$.next(true);
+      }
+    });
   }
 
   /**
@@ -83,6 +83,7 @@ export class QueueHeaderComponent implements OnInit {
   private getTrackSubscription(): void {
     let first = true;
     this.mpdService.currentTrack.subscribe((queueTrack) => {
+      console.log("got a track", queueTrack)
       this.currentTrack = queueTrack;
       if (first || queueTrack.changed) {
         first = false;
@@ -96,7 +97,7 @@ export class QueueHeaderComponent implements OnInit {
    */
   private buildMessageReceiver(): void {
     this.messageService.message
-      .pipe(filter((msg) => msg.type === InternalMessageType.UpdateCover))
-      .subscribe(() => this.updateCover());
+    .pipe(filter((msg) => msg.type === InternalMessageType.UpdateCover))
+    .subscribe(() => this.updateCover());
   }
 }

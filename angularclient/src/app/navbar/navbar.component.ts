b5162ffe0d191bdffa509b2022ff8a4d4ab7ac50
Rainer Hihn <hihn.rainer@gmail.com>
2021-03-05 19:41:53 +0100

    Increased help modal height

--- angularclient/src/app/navbar/navbar.component.ts
+++ angularclient/src/app/navbar/navbar.component.ts
@@ -1,15 +1,15 @@
-import { Component, HostListener } from "@angular/core";
+import {Component, HostListener} from "@angular/core";
 
-import { RxStompService } from "@stomp/ng2-stompjs";
-import { Router } from "@angular/router";
-import { SettingsService } from "../shared/services/settings.service";
-import { BehaviorSubject, Observable } from "rxjs";
-import { MpdCommands } from "../shared/mpd/mpd-commands.enum";
-import { MpdService } from "../shared/services/mpd.service";
-import { WebSocketService } from "../shared/services/web-socket.service";
-import { MpdModeService } from "../shared/services/mpd-mode.service";
-import { MatDialog } from "@angular/material/dialog";
-import { HelpModalComponent } from "./help-dialog/help-modal.component";
+import {RxStompService} from "@stomp/ng2-stompjs";
+import {Router} from "@angular/router";
+import {SettingsService} from "../shared/services/settings.service";
+import {BehaviorSubject, Observable} from "rxjs";
+import {MpdCommands} from "../shared/mpd/mpd-commands.enum";
+import {MpdService} from "../shared/services/mpd.service";
+import {WebSocketService} from "../shared/services/web-socket.service";
+import {MpdModeService} from "../shared/services/mpd-mode.service";
+import {MatDialog} from "@angular/material/dialog";
+import {HelpModalComponent} from "./help-dialog/help-modal.component";
 
 @Component({
   selector: "app-navbar",
@@ -23,18 +23,18 @@ export class NavbarComponent {
   private helpModalOpen = new BehaviorSubject(false);
 
   constructor(
-    private dialog: MatDialog,
-    private mpdModeService: MpdModeService,
-    private mpdService: MpdService,
-    private router: Router,
-    private rxStompService: RxStompService,
-    private settingsService: SettingsService,
-    private webSocketService: WebSocketService
+      private dialog: MatDialog,
+      private mpdModeService: MpdModeService,
+      private mpdService: MpdService,
+      private router: Router,
+      private rxStompService: RxStompService,
+      private settingsService: SettingsService,
+      private webSocketService: WebSocketService
   ) {
     this.isDarkTheme = this.settingsService.isDarkTheme;
     this.connState = rxStompService.connectionState$;
     this.mpdService.currentState.subscribe(
-      (state) => (this.currentState = state)
+        (state) => (this.currentState = state)
     );
   }
 
@@ -44,14 +44,14 @@ export class NavbarComponent {
 
     /* We ignore keys coming from input fields */
     if (
-      inputElement.tagName === "MAT-SLIDER" ||
-      inputElement.tagName === "INPUT"
+        inputElement.tagName === "MAT-SLIDER" ||
+        inputElement.tagName === "INPUT"
     ) {
       return;
     }
 
     switch (event.key) {
-      // Player controls
+        // Player controls
       case "<":
       case "ArrowLeft": // Left: Previous track
         this.webSocketService.send(MpdCommands.SET_PREV);
@@ -64,7 +64,7 @@ export class NavbarComponent {
       case " ": // Space or 'p': pause
         this.togglePause();
         break;
-      // Navigate to another view
+        // Navigate to another view
       case "1":
         void this.router.navigate(["/"]);
         break;
@@ -77,7 +77,7 @@ export class NavbarComponent {
       case "4":
         void this.router.navigate(["/settings"]);
         break;
-      // MPD modes controls
+        // MPD modes controls
       case "r":
         this.mpdModeService.toggleCtrlFromInput("repeat");
         break;
@@ -93,12 +93,12 @@ export class NavbarComponent {
       case "x":
         this.mpdModeService.toggleCtrlFromInput("crossfade");
         break;
-      // Display help modal
+        // Display help modal
       case "h":
       case "?":
         this.openHelpModal();
         break;
-      // Decrease / increase volume
+        // Decrease / increase volume
       case "+":
         // this.mpdService.volumeSetter.next(1);
         this.mpdService.increaseVolume();
@@ -117,7 +117,7 @@ export class NavbarComponent {
       this.helpModalOpen.next(true);
       const dialogRef = this.dialog.open(HelpModalComponent, {
         autoFocus: true,
-        height: "60%",
+        height: "75%",
         width: "80%",
         panelClass: this.settingsService.isDarkTheme$.value ? "dark-theme" : "",
       });

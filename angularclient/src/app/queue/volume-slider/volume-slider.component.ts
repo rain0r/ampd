174b3d354565f14059ceb521d0c6fd3b8edcfe52
Rainer Hihn <hihn.rainer@gmail.com>
2021-03-05 17:22:28 +0100

    Release focus after changing volume

--- angularclient/src/app/queue/volume-slider/volume-slider.component.ts
+++ angularclient/src/app/queue/volume-slider/volume-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Renderer2 } from "@angular/core";
 import { MatSliderChange } from "@angular/material/slider";
 import { WebSocketService } from "../../shared/services/web-socket.service";
 import { MpdService } from "../../shared/services/mpd.service";
@@ -10,11 +10,12 @@ import { MpdCommands } from "../../shared/mpd/mpd-commands.enum";
   styleUrls: ["./volume-slider.component.scss"],
 })
 export class VolumeSliderComponent {
-  volume = -1;
+  volume = 0;
 
   constructor(
-    private webSocketService: WebSocketService,
-    private mpdService: MpdService
+    private mpdService: MpdService,
+    private renderer: Renderer2,
+    private webSocketService: WebSocketService
   ) {
     mpdService.volume.subscribe((volume) => (this.volume = volume));
   }
@@ -23,5 +24,6 @@ export class VolumeSliderComponent {
     this.webSocketService.sendData(MpdCommands.SET_VOLUME, {
       value: event.value,
     });
+    event.source.blur();
   }
 }

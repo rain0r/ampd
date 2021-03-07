b0f9e38e163228cb288c2a84c653e7a1e9cf5bd6
Rainer Hihn <hihn.rainer@gmail.com>
2021-03-07 14:54:40 +0100

    Formatting.

--- angularclient/src/app/browse/playlists/playlists.component.ts
+++ angularclient/src/app/browse/playlists/playlists.component.ts
@@ -1,15 +1,15 @@
-import {Component, Input} from "@angular/core";
-import {Playlist} from "../../shared/messages/incoming/playlist-impl";
-import {MessageService} from "../../shared/services/message.service";
-import {NotificationService} from "../../shared/services/notification.service";
-import {WebSocketService} from "../../shared/services/web-socket.service";
-import {Filterable} from "../filterable";
-import {MatDialog} from "@angular/material/dialog";
-import {PlaylistInfoModalComponent} from "./playlist-info-modal/playlist-info-modal.component";
-import {MpdCommands} from "../../shared/mpd/mpd-commands.enum";
-import {SettingsService} from "../../shared/services/settings.service";
-import {DeviceDetectorService} from "ngx-device-detector";
-import {MatDialogConfig} from "@angular/material/dialog/dialog-config";
+import { Component, Input } from "@angular/core";
+import { Playlist } from "../../shared/messages/incoming/playlist-impl";
+import { MessageService } from "../../shared/services/message.service";
+import { NotificationService } from "../../shared/services/notification.service";
+import { WebSocketService } from "../../shared/services/web-socket.service";
+import { Filterable } from "../filterable";
+import { MatDialog } from "@angular/material/dialog";
+import { PlaylistInfoModalComponent } from "./playlist-info-modal/playlist-info-modal.component";
+import { MpdCommands } from "../../shared/mpd/mpd-commands.enum";
+import { SettingsService } from "../../shared/services/settings.service";
+import { DeviceDetectorService } from "ngx-device-detector";
+import { MatDialogConfig } from "@angular/material/dialog/dialog-config";
 
 @Component({
   selector: "app-playlists",
@@ -20,12 +20,12 @@ export class PlaylistsComponent extends Filterable {
   @Input() playlistQueue: Playlist[] = [];
 
   constructor(
-      private deviceService: DeviceDetectorService,
-      private dialog: MatDialog,
-      private messageService: MessageService,
-      private notificationService: NotificationService,
-      private settingsService: SettingsService,
-      private webSocketService: WebSocketService
+    private deviceService: DeviceDetectorService,
+    private dialog: MatDialog,
+    private messageService: MessageService,
+    private notificationService: NotificationService,
+    private settingsService: SettingsService,
+    private webSocketService: WebSocketService
   ) {
     super(messageService);
   }
@@ -39,17 +39,14 @@ export class PlaylistsComponent extends Filterable {
 
   onPlaylistInfo($event: MouseEvent, playlist: Playlist): void {
     $event.stopPropagation();
-    const width =
-        this.deviceService.isMobile()
-            ? "100%"
-            : "70%";
+    const width = this.deviceService.isMobile() ? "100%" : "70%";
 
     const options: MatDialogConfig = {
-      maxWidth: '100vw',
+      maxWidth: "100vw",
       panelClass: this.settingsService.isDarkTheme$.value ? "dark-theme" : "",
       width: width,
       data: playlist,
-    }
+    };
 
     if (this.deviceService.isMobile()) {
       options["height"] = "100%";

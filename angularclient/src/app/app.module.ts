494d1f955909dd8a62f05457f1d7371cab58bfed
Rainer Hihn <hihn.rainer@gmail.com>
2021-03-07 17:05:56 +0100

    M  angularclient/angular.json A  angularclient/angular2.json M  angularclient/src/app/app.module.ts A  angularclient/src/app/package.json

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -1,71 +1,68 @@
-import { NgModule } from "@angular/core";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { CoverModalComponent } from "src/app/queue/cover-modal/cover-modal.component";
-import { AppComponent } from "./app.component";
-import { BrowseComponent } from "./browse/browse.component";
-import { DirectoriesComponent } from "./browse/directories/directories.component";
-import { BrowseNavigationComponent } from "./browse/navigation/browse-navigation.component";
-import { PlaylistsComponent } from "./browse/playlists/playlists.component";
-import { TracksComponent } from "./browse/tracks/tracks.component";
-import { ControlPanelComponent } from "./queue/control-panel/control-panel.component";
-import { MpdModeComponent } from "./queue/mpd-modes/mpd-mode.component";
-import { QueueHeaderComponent } from "./queue/queue-header/queue-header.component";
-import { QueueComponent } from "./queue/queue.component";
-import { TrackProgressComponent } from "./queue/track-progress/track-progress.component";
-import { TrackTableComponent } from "./queue/track-table/track-table.component";
-import { VolumeSliderComponent } from "./queue/volume-slider/volume-slider.component";
-import { SearchComponent } from "./search/search.component";
-import { SettingsComponent } from "./settings/settings.component";
-import { EncodeURIComponentPipe } from "./shared/pipes/EncodeURI";
-import { DirectoryFilterPipe } from "./shared/pipes/filter/DirectoryFilter";
-import { PlaylistFilterPipe } from "./shared/pipes/filter/PlaylistFilter";
-import { SecondsToMmSsPipe } from "./shared/pipes/SecondsToMmSs";
-import { AppRoutingModule } from "./app-routing.module";
-import { BrowseService } from "./shared/services/browse.service";
-import { MessageService } from "./shared/services/message.service";
-import { NotificationService } from "./shared/services/notification.service";
-import { SecondsToHhMmSsPipe } from "./shared/pipes/SecondsToHhMmSs";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { HttpClientModule } from "@angular/common/http";
-import { MatIconModule } from "@angular/material/icon";
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatSliderModule } from "@angular/material/slider";
-import { MatDividerModule } from "@angular/material/divider";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatButtonModule } from "@angular/material/button";
-import { MatButtonToggleModule } from "@angular/material/button-toggle";
-import { MatTableModule } from "@angular/material/table";
-import { MatCardModule } from "@angular/material/card";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatInputModule } from "@angular/material/input";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { MatListModule } from "@angular/material/list";
-import { MatSortModule } from "@angular/material/sort";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { SavePlaylistModalComponent } from "./queue/save-playlist-modal/save-playlist-modal.component";
-import { PlaylistInfoModalComponent } from "./browse/playlists/playlist-info-modal/playlist-info-modal.component";
-import { TrackDataTableComponent } from "./shared/track-table/track-data-table.component";
-import { NgxFilesizeModule } from "ngx-filesize";
-import {
-  InjectableRxStompConfig,
-  RxStompService,
-  rxStompServiceFactory,
-} from "@stomp/ng2-stompjs";
-import { WebSocketService } from "./shared/services/web-socket.service";
-import { AmpdRxStompConfig } from "./ampd-rx-stomp-config.service";
-import { ReplaceNullWithTextPipe } from "./shared/pipes/replace-null-with-text.pipe";
-import { ThemeComponent } from "./settings/theme/theme.component";
-import { DisplayCoverComponent } from "./settings/display-cover/display-cover.component";
-import { DisplaySavePlaylistComponent } from "./settings/display-save-playlist/display-save-playlist.component";
-import { TabTitleComponent } from "./settings/tab-title/tab-title.component";
-import { HelpModalComponent } from "./navbar/help-dialog/help-modal.component";
-import { MapEntriesPipe } from "./shared/pipes/map-entries.pipe";
+import {NgModule} from "@angular/core";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CoverModalComponent} from "src/app/queue/cover-modal/cover-modal.component";
+import {AppComponent} from "./app.component";
+import {BrowseComponent} from "./browse/browse.component";
+import {DirectoriesComponent} from "./browse/directories/directories.component";
+import {BrowseNavigationComponent} from "./browse/navigation/browse-navigation.component";
+import {PlaylistsComponent} from "./browse/playlists/playlists.component";
+import {TracksComponent} from "./browse/tracks/tracks.component";
+import {ControlPanelComponent} from "./queue/control-panel/control-panel.component";
+import {MpdModeComponent} from "./queue/mpd-modes/mpd-mode.component";
+import {QueueHeaderComponent} from "./queue/queue-header/queue-header.component";
+import {QueueComponent} from "./queue/queue.component";
+import {TrackProgressComponent} from "./queue/track-progress/track-progress.component";
+import {TrackTableComponent} from "./queue/track-table/track-table.component";
+import {VolumeSliderComponent} from "./queue/volume-slider/volume-slider.component";
+import {SearchComponent} from "./search/search.component";
+import {SettingsComponent} from "./settings/settings.component";
+import {EncodeURIComponentPipe} from "./shared/pipes/EncodeURI";
+import {DirectoryFilterPipe} from "./shared/pipes/filter/DirectoryFilter";
+import {PlaylistFilterPipe} from "./shared/pipes/filter/PlaylistFilter";
+import {SecondsToMmSsPipe} from "./shared/pipes/SecondsToMmSs";
+import {AppRoutingModule} from "./app-routing.module";
+import {BrowseService} from "./shared/services/browse.service";
+import {MessageService} from "./shared/services/message.service";
+import {NotificationService} from "./shared/services/notification.service";
+import {SecondsToHhMmSsPipe} from "./shared/pipes/SecondsToHhMmSs";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {HttpClientModule} from "@angular/common/http";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatDividerModule} from "@angular/material/divider";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatTableModule} from "@angular/material/table";
+import {MatCardModule} from "@angular/material/card";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatInputModule} from "@angular/material/input";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatListModule} from "@angular/material/list";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {SavePlaylistModalComponent} from "./queue/save-playlist-modal/save-playlist-modal.component";
+import {PlaylistInfoModalComponent} from "./browse/playlists/playlist-info-modal/playlist-info-modal.component";
+import {TrackDataTableComponent} from "./shared/track-table/track-data-table.component";
+import {NgxFilesizeModule} from "ngx-filesize";
+import {InjectableRxStompConfig, RxStompService, rxStompServiceFactory,} from "@stomp/ng2-stompjs";
+import {WebSocketService} from "./shared/services/web-socket.service";
+import {AmpdRxStompConfig} from "./ampd-rx-stomp-config.service";
+import {ReplaceNullWithTextPipe} from "./shared/pipes/replace-null-with-text.pipe";
+import {ThemeComponent} from "./settings/theme/theme.component";
+import {DisplayCoverComponent} from "./settings/display-cover/display-cover.component";
+import {DisplaySavePlaylistComponent} from "./settings/display-save-playlist/display-save-playlist.component";
+import {TabTitleComponent} from "./settings/tab-title/tab-title.component";
+import {HelpModalComponent} from "./navbar/help-dialog/help-modal.component";
+import {MapEntriesPipe} from "./shared/pipes/map-entries.pipe";
+
 
 @NgModule({
   declarations: [
@@ -149,4 +146,5 @@ import { MapEntriesPipe } from "./shared/pipes/map-entries.pipe";
   entryComponents: [CoverModalComponent],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}

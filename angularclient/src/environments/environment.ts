96a80f2ec4b59d4270fb824c7fdf463993573543
Rainer Hihn <hihn.rainer@gmail.com>
2021-03-09 18:26:39 +0100

    Formatting.

--- angularclient/src/environments/environment.ts
+++ angularclient/src/environments/environment.ts
@@ -2,9 +2,9 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// const AMPD_URL = "punica:8003";
+const AMPD_URL = "punica:8003";
 // const AMPD_URL = "punica:8083";
-const AMPD_URL = "192.168.178.50:8080";
+// const AMPD_URL = "192.168.178.50:8080";
 
 export const environment = {
   production: false,

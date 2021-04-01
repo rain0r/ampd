639419e4b112d060ad4b62b868df812f7b9e4232
Rainer Hihn <hihn.rainer@gmail.com>
2021-04-01 23:02:18 +0200

    Added a letter filter for directories

--- angularclient/src/app/browse/directories/directories.component.ts
+++ angularclient/src/app/browse/directories/directories.component.ts
@@ -1,36 +1,43 @@
-import { Component, Input } from "@angular/core";
-import { MessageService } from "../../shared/services/message.service";
-import { NotificationService } from "../../shared/services/notification.service";
-import { Filterable } from "../filterable";
+import {Component, Input, OnInit} from "@angular/core";
+import {MessageService} from "../../shared/services/message.service";
+import {NotificationService} from "../../shared/services/notification.service";
+import {Filterable} from "../filterable";
 
-import { ResponsiveCoverSizeService } from "../../shared/services/responsive-cover-size.service";
-import { Observable } from "rxjs";
-import { Directory } from "../../shared/messages/incoming/directory";
-import { MatDialog } from "@angular/material/dialog";
-import { ControlPanelService } from "../../shared/services/control-panel.service";
-import { QueueService } from "../../shared/services/queue.service";
+import {ResponsiveCoverSizeService} from "../../shared/services/responsive-cover-size.service";
+import {Observable} from "rxjs";
+import {Directory} from "../../shared/messages/incoming/directory";
+import {MatDialog} from "@angular/material/dialog";
+import {ControlPanelService} from "../../shared/services/control-panel.service";
+import {QueueService} from "../../shared/services/queue.service";
 
 @Component({
   selector: "app-directories",
   templateUrl: "./directories.component.html",
   styleUrls: ["./directories.component.scss"],
 })
-export class DirectoriesComponent extends Filterable {
+export class DirectoriesComponent extends Filterable implements OnInit {
   @Input() dirQueue: Directory[] = [];
   @Input() dirQp = "/";
   coverSizeClass: Observable<string>;
+  filterVisible = false;
   maxCoversDisplayed = 50;
+  filterByStartCharValue = "";
+  letters: Set<string> = new Set<string>();
 
   constructor(
-    private controlPanelService: ControlPanelService,
-    private dialog: MatDialog,
-    private messageService: MessageService,
-    private notificationService: NotificationService,
-    private queueService: QueueService,
-    private responsiveCoverSizeService: ResponsiveCoverSizeService
+      private controlPanelService: ControlPanelService,
+      private dialog: MatDialog,
+      private messageService: MessageService,
+      private notificationService: NotificationService,
+      private queueService: QueueService,
+      private responsiveCoverSizeService: ResponsiveCoverSizeService
   ) {
     super(messageService);
     this.coverSizeClass = responsiveCoverSizeService.getCoverCssClass();
+  }
+
+  ngOnInit() {
+    this.buildLetters();
   }
 
   onPlayDir($event: MouseEvent, dir: string): void {
@@ -48,4 +55,25 @@ export class DirectoriesComponent extends Filterable {
     this.queueService.addDir(dir);
     this.notificationService.popUp(`Added dir: "${dir}"`);
   }
+
+  toggleFilter() {
+    this.filterVisible = !this.filterVisible;
+  }
+
+  setStartLetterFilter(letter: string) {
+    if (letter === "") {
+      this.filterVisible=false;
+    }
+    this.filterByStartCharValue = letter.substr(0, 1).toUpperCase();
+  }
+
+  private buildLetters() {
+    console.log("buildLetters");
+    const mySet1 = new Set<string>()
+    this.dirQueue.forEach((val) => {
+      mySet1.add(val.path.substr(0, 1).toUpperCase());
+    });
+    this.letters = mySet1;
+  }
+
 }

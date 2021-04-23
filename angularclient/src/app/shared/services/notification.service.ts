073202a77b076418ac2958b1f48644cfb360be02
Rainer Hihn <hihn.rainer@gmail.com>
2021-04-23 09:20:18 +0200

    Fix: Add dir with ampersand in name

--- angularclient/src/app/shared/services/notification.service.ts
+++ angularclient/src/app/shared/services/notification.service.ts
@@ -7,7 +7,7 @@ export class NotificationService {
 
   popUp(message: string, isErrorMsg = false): void {
     const duration = isErrorMsg ? 10000 : 2000;
-    this.snackBar.open(message, "Close", {
+    this.snackBar.open(decodeURIComponent(message), "Close", {
       duration: duration,
     });
   }

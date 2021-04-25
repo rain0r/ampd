960dd294ad364dd4df549082290369ed2daa8a8e
Rainer Hihn <hihn.rainer@gmail.com>
2021-04-25 21:54:46 +0200

    Fix: display MPD Modes panel

--- angularclient/src/app/shared/messages/incoming/state-msg-payload.ts
+++ angularclient/src/app/shared/messages/incoming/state-msg-payload.ts
@@ -3,7 +3,7 @@ import { Track } from "./track";
 import { ServerStatus } from "./server-status";
 
 export interface StateMsgPayload {
-  mpdModesPanel: MpdModesPanel;
+  mpdModesPanelMsg: MpdModesPanel;
   currentTrack: Track;
   serverStatus: ServerStatus;
 }

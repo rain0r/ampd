2e1ab955ed4ef5b526d79db43b3ea9388930092f
Rainer Hihn <hihn.rainer@gmail.com>
2022-01-19 21:36:13 +0100

    M src/main/java/org/hihn/ampd/server/service/AlbumService.java

--- src/main/java/org/hihn/ampd/server/service/AlbumService.java
+++ src/main/java/org/hihn/ampd/server/service/AlbumService.java
@@ -3,6 +3,7 @@ package org.hihn.ampd.server.service;
 import java.util.Collection;
 import java.util.stream.Collectors;
 import org.bff.javampd.album.MPDAlbum;
+import org.bff.javampd.artist.MPDArtist;
 import org.bff.javampd.server.MPD;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,15 @@ public class AlbumService {
     Collection<MPDAlbum> foo = mpd.getMusicDatabase().getAlbumDatabase().listAllAlbums().stream()
         .filter(album -> !album.getName().isEmpty())
         .filter(album -> !album.getArtistName().isEmpty()).collect(Collectors.toList());
+
+    foo.clear();
+    Collection<MPDArtist> artists = mpd.getMusicDatabase().getArtistDatabase().listAllArtists();
+    for (MPDArtist artist : artists) {
+      foo.addAll(mpd.getMusicDatabase().getAlbumDatabase().listAlbumsByArtist(artist).stream()
+          .filter(album -> !album.getName().isEmpty())
+          .filter(album -> !album.getArtistName().isEmpty()).collect(Collectors.toList()));
+    }
+
     return foo;
   }
 }

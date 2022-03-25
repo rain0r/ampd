d549d6b3c2697e819ebe5f45eaaf4baafd150d51
Rainer Hihn <hihn.rainer@gmail.com>
2022-03-25 19:34:15 +0100

    Implemented a filter function

--- angularclient/src/app/browse/albums/albums.component.ts
+++ angularclient/src/app/browse/albums/albums.component.ts
@@ -1,7 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Observable } from "rxjs";
-import { distinctUntilChanged, map, tap } from "rxjs/operators";
+import { BehaviorSubject, combineLatest, Observable, of, Subject } from "rxjs";
+import {
+  bufferTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap,
+  tap,
+} from "rxjs/operators";
 import { MpdAlbum } from "src/app/shared/models/http/album";
 import { AlbumsService } from "src/app/shared/services/albums.service";
 
@@ -10,18 +17,69 @@ import { AlbumsService } from "src/app/shared/services/albums.service";
   templateUrl: "./albums.component.html",
   styleUrls: ["./albums.component.scss"],
 })
-export class AlbumsComponent {
+export class AlbumsComponent implements OnInit {
+  @ViewChild("filterInputElem") filterInputElem?: ElementRef;
   albums: Observable<MpdAlbum[]> | null = null;
-  page: Observable<number>;
+  filter = "";
+  page: Observable<number> = new Observable<number>();
+  isLoading = new BehaviorSubject(true);
+  private inputSetter$ = new Subject<string>();
 
   constructor(
     private albumService: AlbumsService,
     private activatedRoute: ActivatedRoute
-  ) {
+  ) {}
+
+  ngOnInit(): void {
+    this.buildPageListener();
+    this.buildInputListener();
+    this.loadData();
+  }
+
+  applyFilter(eventTarget: EventTarget | null): void {
+    const inputValue = (<HTMLInputElement>eventTarget).value;
+    if (inputValue) {
+      this.inputSetter$.next(inputValue);
+    } else {
+      this.loadData();
+    }
+  }
+
+  private buildInputListener() {
+    const searchInput = this.inputSetter$.asObservable().pipe(
+      bufferTime(1000),
+      distinctUntilChanged(),
+      filter((times: string[]) => times.length > 0),
+      map((input: string[]) => input[input.length - 1])
+    );
+
+    combineLatest([this.page, searchInput])
+      .pipe(
+        switchMap(([page, searchInput]) => {
+          console.log("page:", page);
+          console.log("searchInput:", searchInput);
+          this.albums = null;
+          this.isLoading.next(true);
+          return this.albumService.getAlbums(page, searchInput);
+        }),
+        tap((albums) => (this.albums = of(albums)))
+      )
+      .subscribe((result) => {
+        this.isLoading.next(false);
+      });
+  }
+
+  private loadData() {
+    this.isLoading.next(true);
+    this.page.subscribe(
+      (page) => (this.albums = this.albumService.getAlbums(page))
+    );
+  }
+
+  private buildPageListener() {
     this.page = this.activatedRoute.queryParamMap.pipe(
       map((qp) => parseInt(qp.get("page") || "1")),
-      distinctUntilChanged(),
-      tap((page) => (this.albums = this.albumService.getAlbums(page)))
+      distinctUntilChanged()
     );
   }
 }

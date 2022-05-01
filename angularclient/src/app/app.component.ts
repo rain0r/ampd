2dd46b4e626c75ca52d5f50b28f2f2af5f27705e
Rainer Hihn <hihn.rainer@gmail.com>
2022-05-01 15:03:34 +0200

    Refresh ampd after some time to prevent tab freezes

--- angularclient/src/app/app.component.ts
+++ angularclient/src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+// RxJS v6+
+import { interval } from "rxjs";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const period =
+      4 * // hours
+      60 * // minutes
+      1000; // miliseconds
+
+    // Reload the tab (it sometimes freezes after >12 hours)
+    const source = interval(period);
+    source.subscribe(() => window.location.reload());
+  }
+}

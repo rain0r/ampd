7af15446312613aa366e444a001465a17acb824b
Rainer Hihn <hihn.rainer@gmail.com>
2022-05-01 18:44:46 +0200

    Reset hideDelay

--- angularclient/src/app/shared/custom-tooltip-defaults.ts
+++ angularclient/src/app/shared/custom-tooltip-defaults.ts
@@ -2,6 +2,6 @@ import { MatTooltipDefaultOptions } from "@angular/material/tooltip";
 
 export const customTooltipDefaults: MatTooltipDefaultOptions = {
   showDelay: 1000,
-  hideDelay: 1000,
+  hideDelay: 0,
   touchendHideDelay: 0,
 };

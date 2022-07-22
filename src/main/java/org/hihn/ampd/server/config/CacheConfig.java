5a424eb7fa992a9455b9673c8e6ec5154c490390
Rainer Hihn <hihn.rainer@gmail.com>
2022-07-22 22:44:31 +0200

    Renamed CacheConfig

--- src/main/java/org/hihn/ampd/server/config/CacheConfig.java
+++ src/main/java/org/hihn/ampd/server/config/CacheConfig.java
@@ -1,17 +1,16 @@
 package org.hihn.ampd.server.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.concurrent.TimeUnit;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.TimeUnit;
-
 @EnableCaching
 @Configuration
-public class CaffeineConfig {
+public class CacheConfig {
 
 	@Bean
 	public Caffeine caffeineConfig() {

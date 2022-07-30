01b7c72b83f958384e58782877b3b1f525392f7a
Rainer Hihn <hihn.rainer@gmail.com>
2022-07-30 10:56:20 +0200

    Removed surplus colon.

--- angularclient/src/app/shared/pipes/seconds-to-hh-mm-ss.pipe.ts
+++ angularclient/src/app/shared/pipes/seconds-to-hh-mm-ss.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({ name: "secondsToHhMmSs" })
 export class SecondsToHhMmSsPipe implements PipeTransform {
-  transform(d: number, labels = [":", ":", ":"]): string {
+  transform(d: number, labels = [":", ":", ""]): string {
     const h = Math.floor(d / 3600);
     const m = Math.floor((d % 3600) / 60);
     const s = Math.floor((d % 3600) % 60);

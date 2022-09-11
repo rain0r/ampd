57b9e7d0561415197c013478a6ce1976c9310f0f
Rainer Hihn <hihn.rainer@gmail.com>
2022-09-11 15:14:57 +0200

    Use substring instead of substr

--- angularclient/src/app/shared/pipes/camel-case-title.pipe.ts
+++ angularclient/src/app/shared/pipes/camel-case-title.pipe.ts
@@ -15,7 +15,7 @@ export class CamelCaseTitlePipe implements PipeTransform {
 
     if (value.toLowerCase().startsWith("musicbrainz")) {
       return value.replace(/_/g, " ").replace(/\w\S*/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
       });
     }
 

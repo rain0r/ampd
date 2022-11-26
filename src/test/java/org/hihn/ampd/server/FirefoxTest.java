a717cf30d866a67f710d4d0fcb2cc85fac6a4aa2
Rainer Hihn <hihn.rainer@gmail.com>
2022-11-26 19:46:44 +0100

    M pom.xml AM src/test/java/org/hihn/ampd/server/FirefoxTest.java

---
+++ src/test/java/org/hihn/ampd/server/FirefoxTest.java
@@ -0,0 +1,27 @@
+package org.hihn.ampd.server;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirefoxTest {
+
+	@Test
+	void addition() {
+		assertEquals(2, 1 + 1);
+	}
+
+	@Test
+	void ff() {
+		FirefoxOptions options = new FirefoxOptions();
+		options.setHeadless(true); // <-- headless set here
+		FirefoxDriver driver = new FirefoxDriver(options);
+		driver.get("https://hihn.org");
+		String title = driver.getTitle();
+
+		System.out.println(title);
+	}
+
+}

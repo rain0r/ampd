1bad65b3179d41b83b8c1baf88696e93032d78de
Rainer Hihn <hihn.rainer@gmail.com>
2022-11-26 20:48:26 +0100

    M  pom.xml M  src/test/java/org/hihn/ampd/server/FirefoxTest.java A  src/test/resources/application.properties

--- src/test/java/org/hihn/ampd/server/FirefoxTest.java
+++ src/test/java/org/hihn/ampd/server/FirefoxTest.java
@@ -1,21 +1,24 @@
 package org.hihn.ampd.server;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@ExtendWith(SpringExtension.class)
+// @TestPropertySource(locations = "application.properties")
+@TestPropertySource("classpath:application.properties")
 public class FirefoxTest {
 
-	@Test
-	void addition() {
-		assertEquals(2, 1 + 1);
-	}
+	@Value("${webdriver.gecko.driver}")
+	private String gecko;
 
 	@Test
 	void ff() {
-		System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+		System.setProperty("webdriver.gecko.driver", gecko);
 		FirefoxOptions options = new FirefoxOptions();
 		options.setHeadless(true); // <-- headless set here
 		FirefoxDriver driver = new FirefoxDriver(options);

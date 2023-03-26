02691a56ec057a616ac4eb754ba4ec646dcedcd4
Rainer Hihn <hihn.rainer@gmail.com>
2023-03-26 21:42:27 +0200

    Made playlist info dialog more mobile-friendly

--- src/main/java/org/hihn/ampd/server/Constants.java
+++ src/main/java/org/hihn/ampd/server/Constants.java
@@ -2,10 +2,11 @@ package org.hihn.ampd.server;
 
 public interface Constants {
 
-	String ALBUM_CACHE = "AlbumService";
+    String ALBUM_CACHE = "AlbumService";
 
-	String GENRE_CACHE = "GenreService";
+    String GENRE_CACHE = "GenreService";
 
-	String SEARCH_CACHE = "SearchService";
+    String SEARCH_CACHE = "SearchService";
 
+    String PLAYLIST_CACHE = "PlaylistService";
 }
--- src/main/java/org/hihn/ampd/server/service/PlaylistService.java
+++ src/main/java/org/hihn/ampd/server/service/PlaylistService.java
@@ -9,6 +9,8 @@ import org.hihn.ampd.server.model.PlaylistInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.support.PagedListHolder;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,90 +20,95 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.hihn.ampd.server.Constants.PLAYLIST_CACHE;
+
 /**
  * Provides methods to manage playlists.
  */
 @Service
+@CacheConfig(cacheNames = {PLAYLIST_CACHE})
 public class PlaylistService {
 
-	private static final Logger LOG = LoggerFactory.getLogger(PlaylistService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PlaylistService.class);
 
-	private static final int TABLE_SIZE = 10;
+    private static final int TABLE_SIZE = 10;
 
-	private final MPD mpd;
+    private final MPD mpd;
 
-	private final AmpdSettings ampdSettings;
+    private final AmpdSettings ampdSettings;
 
-	public PlaylistService(MPD mpd, AmpdSettings ampdSettings) {
-		this.mpd = mpd;
-		this.ampdSettings = ampdSettings;
-	}
+    public PlaylistService(MPD mpd, AmpdSettings ampdSettings) {
+        this.mpd = mpd;
+        this.ampdSettings = ampdSettings;
+    }
 
-	/**
-	 * Deletes a playlist.
-	 * @param playlistName Name of the playlist to delete.
-	 */
-	public void deleteByName(String playlistName) {
-		if (ampdSettings.isDeletePlaylists()) {
-			mpd.getPlaylist().deletePlaylist(playlistName);
-		}
-	}
+    /**
+     * Deletes a playlist.
+     *
+     * @param playlistName Name of the playlist to delete.
+     */
+    public void deleteByName(String playlistName) {
+        if (ampdSettings.isDeletePlaylists()) {
+            mpd.getPlaylist().deletePlaylist(playlistName);
+        }
+    }
 
-	/**
-	 * Saves the current queue to a playlist on the MPD server.
-	 * @param playlistName Name of the new playlist.
-	 * @return A {@link SavePlaylistResponse}-object containing the search results.
-	 */
-	public SavePlaylistResponse savePlaylist(String playlistName) {
-		SavePlaylistResponse response = new SavePlaylistResponse();
-		response.setPlaylistName(playlistName);
-		if (ampdSettings.isCreatePlaylists()) {
-			try {
-				response.setSuccess(mpd.getPlaylist().savePlaylist(playlistName));
-			}
-			catch (MPDConnectionException e) {
-				response.setSuccess(false);
-				response.setMessage(e.getMessage());
-				LOG.error("Failed to create playlist: {}", playlistName, e);
-			}
-		}
-		else {
-			response.setSuccess(false);
-			response.setMessage("Saving new playlists is disabled on the server.");
-		}
-		return response;
-	}
+    /**
+     * Saves the current queue to a playlist on the MPD server.
+     *
+     * @param playlistName Name of the new playlist.
+     * @return A {@link SavePlaylistResponse}-object containing the search results.
+     */
+    public SavePlaylistResponse savePlaylist(String playlistName) {
+        SavePlaylistResponse response = new SavePlaylistResponse();
+        response.setPlaylistName(playlistName);
+        if (ampdSettings.isCreatePlaylists()) {
+            try {
+                response.setSuccess(mpd.getPlaylist().savePlaylist(playlistName));
+            } catch (MPDConnectionException e) {
+                response.setSuccess(false);
+                response.setMessage(e.getMessage());
+                LOG.error("Failed to create playlist: {}", playlistName, e);
+            }
+        } else {
+            response.setSuccess(false);
+            response.setMessage("Saving new playlists is disabled on the server.");
+        }
+        return response;
+    }
 
-	/**
-	 * Gets info about a playlist from the MPD server.
-	 * @param name The name of the playlist.
-	 * @return Info about the specified playlist.
-	 */
-	public Optional<PlaylistInfo> getPlaylistInfo(String name, int pageIndex, Integer pageSize) {
-		Optional<PlaylistInfo> ret = Optional.empty();
-		try {
-			PageImpl<MPDSong> page = getPage(
-					new ArrayList<>(mpd.getMusicDatabase().getPlaylistDatabase().listPlaylistSongs(name)), pageIndex,
-					pageSize);
-			ret = Optional.of(new PlaylistInfo(name, page));
-		}
-		catch (Exception e) {
-			LOG.error("Could not get info about playlist: {}", name, e);
-		}
-		return ret;
-	}
+    /**
+     * Gets info about a playlist from the MPD server.
+     *
+     * @param name The name of the playlist.
+     * @return Info about the specified playlist.
+     */
+    @Cacheable
+    public Optional<PlaylistInfo> getPlaylistInfo(String name, int pageIndex, Integer pageSize) {
+        Optional<PlaylistInfo> ret = Optional.empty();
+        try {
+            PageImpl<MPDSong> page = getPage(
+                    new ArrayList<>(mpd.getMusicDatabase().getPlaylistDatabase().listPlaylistSongs(name)), pageIndex,
+                    pageSize);
+            ret = Optional.of(new PlaylistInfo(name, page));
+        } catch (Exception e) {
+            LOG.error("Could not get info about playlist: {}", name, e);
+        }
+        return ret;
+    }
 
-	private PageImpl<MPDSong> getPage(List<MPDSong> playlists, int pageIndex, Integer pageSize) {
-		Pageable pageable = PageRequest.of(pageIndex, getPageSize(pageSize));
-		PagedListHolder<MPDSong> pages = new PagedListHolder<>(playlists);
-		pages.setPage(pageIndex);
-		pages.setPageSize(getPageSize(pageSize));
-		return new PageImpl<>(pages.getPageList(), pageable, playlists.size());
+    @Cacheable
+    private PageImpl<MPDSong> getPage(List<MPDSong> playlists, int pageIndex, Integer pageSize) {
+        Pageable pageable = PageRequest.of(pageIndex, getPageSize(pageSize));
+        PagedListHolder<MPDSong> pages = new PagedListHolder<>(playlists);
+        pages.setPage(pageIndex);
+        pages.setPageSize(getPageSize(pageSize));
+        return new PageImpl<>(pages.getPageList(), pageable, playlists.size());
 
-	}
+    }
 
-	private int getPageSize(Integer pageSize) {
-		return pageSize == null ? TABLE_SIZE : pageSize;
-	}
+    private int getPageSize(Integer pageSize) {
+        return pageSize == null ? TABLE_SIZE : pageSize;
+    }
 
 }

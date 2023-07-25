7d29e8e5a83afa5616cdcdd15f7d846c0585b985
Rainer Hihn <hihn.rainer@gmail.com>
2023-07-25 21:42:57 +0200

    - Added frontend setting "Jump inside track". - Use user-defined setting to jump. - Added ability to jump multiple times per seek.

--- angularclient/src/app/settings/settings.component.ts
+++ angularclient/src/app/settings/settings.component.ts
@@ -3,10 +3,15 @@ import { MatSlideToggleChange } from "@angular/material/slide-toggle";
 import { Title } from "@angular/platform-browser";
 import { Observable } from "rxjs";
 import { FrontendSettingsService } from "src/app/service/frontend-settings.service";
+import { NotificationService } from "../service/notification.service";
 import { SettingsService } from "../service/settings.service";
 import { AmpdSetting } from "../shared/model/ampd-setting";
 import { FrontendSetting } from "../shared/model/internal/frontend-settings";
 import { MpdSettings } from "../shared/model/mpd-settings";
+
+interface SettingMap {
+  [category: string]: FrontendSetting[];
+}
 
 @Component({
   selector: "app-settings",
@@ -16,17 +21,18 @@ import { MpdSettings } from "../shared/model/mpd-settings";
 export class SettingsComponent {
   mpdSettings: Observable<MpdSettings>;
   ampdSettings: Observable<AmpdSetting[]>;
-  feSettings: FrontendSetting[];
+  feSettings: SettingMap;
 
   constructor(
     private fsService: FrontendSettingsService,
     private settingsService: SettingsService,
-    private titleService: Title
+    private titleService: Title,
+    private notificationService: NotificationService
   ) {
     this.titleService.setTitle("ampd — Settings");
     this.ampdSettings = this.settingsService.getAmpdSettings();
     this.mpdSettings = this.settingsService.getMpdSettings();
-    this.feSettings = this.fsService.loadFrontendSettings();
+    this.feSettings = this.buildFeSettingCategories();
   }
 
   toggleFrontendSetting(name: string, event: MatSlideToggleChange): void {
@@ -35,10 +41,23 @@ export class SettingsComponent {
 
   onSaveBtnClick(name: string, value: string): void {
     this.fsService.save(name, value);
+    this.notificationService.popUp("Saved settings.");
   }
 
   resetFrontendSettings(): void {
     this.fsService.reset();
     window.location.reload();
+  }
+
+  buildFeSettingCategories(): SettingMap {
+    const settingMap = <SettingMap>{};
+    const settings = this.fsService.loadFrontendSettings();
+    for (const setting of settings) {
+      if (!(setting.category in settingMap)) {
+        settingMap[setting.category] = [];
+      }
+      settingMap[setting.category].push(setting);
+    }
+    return settingMap;
   }
 }

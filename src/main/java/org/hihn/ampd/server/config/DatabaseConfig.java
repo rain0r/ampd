d4e21602aed5d48618684b9cffe8a785b472c180
Rainer Hihn <hihn.rainer@gmail.com>
2024-03-18 21:57:25 +0100

    Renamed class to DatabaseConfig

--- src/main/java/org/hihn/ampd/server/config/DatabaseConfig.java
+++ src/main/java/org/hihn/ampd/server/config/DatabaseConfig.java
@@ -8,11 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 
 @Configuration
-public class DataSourceConfig {
+public class DatabaseConfig {
 
 	private final DirService dirService;
 
-	public DataSourceConfig(DirService dirService) {
+	public DatabaseConfig(DirService dirService) {
 		this.dirService = dirService;
 	}
 

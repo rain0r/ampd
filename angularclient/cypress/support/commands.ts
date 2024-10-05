a11c34d750caf3d5f7ec0dd5f0d18c32f6f628da
Rainer Hihn <hihn.rainer@gmail.com>
2024-10-05 22:17:37 +0200

    Tests

--- angularclient/cypress/support/commands.ts
+++ angularclient/cypress/support/commands.ts
@@ -44,7 +44,11 @@ function customCommand(param: any): void {
 
 Cypress.Commands.add("clearQueue", () => {
   cy.visit("/");
-  cy.get('[data-cy="clear-queue-btn"]').click();
+  cy.get('[data-cy="clear-queue-btn"]').then(($btn) => {
+    if (!$btn.is(":disabled")) {
+      cy.wrap($btn).click();
+    }
+  });
   cy.get('[data-cy="cover"]').should("not.exist");
   cy.contains("No tracks.");
 });

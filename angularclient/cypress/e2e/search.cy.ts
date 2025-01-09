9c3e87069666635dcbd27c2b1c92af1252f91b71
Rainer Hihn <hihn.rainer@gmail.com>
2025-01-09 22:43:16 +0100

    Tests for search

---
+++ angularclient/cypress/e2e/search.cy.ts
@@ -0,0 +1,26 @@
+describe("Search Test", () => {
+  it("Search", () => {
+    cy.visit("/search");
+
+    // Should be empty at the beginning
+    cy.get('[data-cy="track-table"] > tbody > tr').should("have.length", 1);
+    cy.get('[data-cy="search-term"]').type("Beatles");
+    cy.get('[data-cy="track-table"] > tbody > tr').should(
+      "have.length.greaterThan",
+      1,
+    );
+  });
+
+  it("Advanced Search", () => {
+    cy.visit("/adv-search");
+
+    // Should be empty at the beginning
+    cy.get('[data-cy="track-table"] > tbody > tr').should("have.length", 1);    
+    cy.get('[data-cy="input-field-artist"]').type("Beatles");
+    cy.get("[data-cy='search-btn']").click();
+    cy.get('[data-cy="track-table"] > tbody > tr').should(
+      "have.length.greaterThan",
+      1,
+    );
+  });
+});

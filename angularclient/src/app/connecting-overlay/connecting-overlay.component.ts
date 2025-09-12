66791789ef5a3c6a3d3bc324ed715c5a24bf5075
Rainer Hihn <hihn.rainer@gmail.com>
2025-09-12 20:47:08 +0200

    Add a blocking overlay while connecting to the server.

---
+++ angularclient/src/app/connecting-overlay/connecting-overlay.component.ts
@@ -0,0 +1,37 @@
+import { AsyncPipe, NgIf } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import {
+  MatDialogActions,
+  MatDialogContent,
+  MatDialogRef,
+} from "@angular/material/dialog";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
+import { interval, map, Observable, take } from "rxjs";
+
+@Component({
+  selector: "app-connecting-overlay",
+  templateUrl: "./connecting-overlay.component.html",
+  styleUrl: "./connecting-overlay.component.css",
+  imports: [
+    MatProgressSpinner,
+    MatDialogContent,
+    MatDialogActions,
+    AsyncPipe,
+    NgIf,
+  ],
+})
+export class ConnectingOverlayComponent {
+  private dialogRef = inject(MatDialogRef<ConnectingOverlayComponent>);
+  counter$: Observable<number>;
+  readonly seconds = 5;
+
+  constructor() {
+    this.counter$ = interval(1000).pipe(
+      take(this.seconds),
+      map((t) => Math.abs(t - this.seconds)),
+    );
+    this.counter$.subscribe({
+      complete: () => this.dialogRef.close(),
+    });
+  }
+}
--- angularclient/src/app/shared/log/log-console.ts
+++ angularclient/src/app/shared/log/log-console.ts
@@ -5,7 +5,7 @@ import { LogPublisher } from "./log-publisher";
 export class LogConsole extends LogPublisher {
   log(entry: LogEntry): Observable<boolean> {
     // Log to console
-    console.log(entry.buildLogString());
+    console.debug(entry.buildLogString());
     return of(true);
   }
 
